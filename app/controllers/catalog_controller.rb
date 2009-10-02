f8a7d4083d7ecaaa8abd49e83ee357594b2902ab
rofreg <rofreg@gmail.com>
2009-10-02 15:45:20 -0400

    more stuffS

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -7,5 +7,19 @@ class CatalogController < ApplicationController
     @equipment_model = EquipmentModel.find(params[:id])
     cart.add_equipment_model(@equipment_model)
     redirect_to root_path
+  rescue ActiveRecord::RecordNotFound 
+    logger.error("Attempt to add invalid equipment model #{params[:id]}") 
+    flash[:notice] = "Invalid equipment_model" 
+    redirect_to root_path
+  end
+  
+  def remove_from_cart
+    @equipment_model = EquipmentModel.find(params[:id])
+    cart.remove_equipment_model(@equipment_model)
+    redirect_to root_path
+  rescue ActiveRecord::RecordNotFound 
+    logger.error("Attempt to remove invalid equipment model #{params[:id]}") 
+    flash[:notice] = "Invalid equipment_model" 
+    redirect_to root_path
   end
 end
