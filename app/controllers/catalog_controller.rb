d21a620a7231aded2bbb24c65059f3d8d06fd918
rofreg <rofreg@gmail.com>
2009-10-07 15:53:30 -0400

    error handling for search

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -32,15 +32,19 @@ class CatalogController < ApplicationController
   end
   
   def search
-    #update dates
-    session[:cart].set_start_date(Date.civil(params[:cart][:"start_date(1i)"].to_i,params[:cart][:"start_date(2i)"].to_i,params[:cart][:"start_date(3i)"].to_i))
-    session[:cart].set_due_date(Date.civil(params[:cart][:"due_date(1i)"].to_i,params[:cart][:"due_date(2i)"].to_i,params[:cart][:"due_date(3i)"].to_i))
+    if params[:category].nil?
+      redirect_to catalog_path
+    else
+      #update dates
+      session[:cart].set_start_date(Date.civil(params[:cart][:"start_date(1i)"].to_i,params[:cart][:"start_date(2i)"].to_i,params[:cart][:"start_date(3i)"].to_i))
+      session[:cart].set_due_date(Date.civil(params[:cart][:"due_date(1i)"].to_i,params[:cart][:"due_date(2i)"].to_i,params[:cart][:"due_date(3i)"].to_i))
     
-    @category = Category.find(params[:category])
-    @equipment_models = @category.equipment_models.select{|e| e.available?(cart.start_date..cart.due_date)}
-    @equipment_models_by_category = @equipment_models.sort_by(&:name).group_by(&:category)
+      @category = Category.find(params[:category])
+      @equipment_models = @category.equipment_models.select{|e| e.available?(cart.start_date..cart.due_date)}
+      @equipment_models_by_category = @equipment_models.sort_by(&:name).group_by(&:category)
     
-    flash.now[:notice] = "The following #{@category.name.pluralize} are available from #{cart.start_date} to #{cart.due_date}:"
-    render :action => :index
+      flash.now[:notice] = "The following #{@category.name.pluralize} are available from #{cart.start_date} to #{cart.due_date}:"
+      render :action => :index
+    end
   end
 end
