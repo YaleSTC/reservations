1a1d4e9b7427b622cd8f5f5296e758e36b7cfea4
Adam Bray <adam.bray@yale.edu>
2009-10-23 08:31:16 -0400

    dont add line breaks when adding tables to description, this is really hackish, should be fixed

--- app/models/equipment_model.rb
+++ app/models/equipment_model.rb
@@ -39,7 +39,10 @@ class EquipmentModel < ActiveRecord::Base
     
     nice_content = "<p>"
     lines.each do |line|
-      nice_content += line + "<br />" unless (line.include? "<table>" or line.include? "<td>")
+      if line.include? "<table>" or line.include? "<td>"
+        nice_content += line
+      else
+        nice_content += line + "<br />" 
     end
     nice_content += "</p>"
   end
