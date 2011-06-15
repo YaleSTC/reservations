6c963ecccc15e53611c1cba2cde2bef5bfb2a4d0
Shafqat Ahmed <shafqat.ahmed@yale.edu>
2011-06-15 15:14:27 -0400

    minor error in equipment_model count of reserved and overdue equipment

--- app/models/equipment_model.rb
+++ app/models/equipment_model.rb
@@ -7,53 +7,53 @@ class EquipmentModel < ActiveRecord::Base
   # has_and_belongs_to_many :reservations
   # has_many :equipment_models_reservations
   has_many :reservations
-  
+
   #associates with itself for accessories/recommended related models
   has_many :accessories_equipment_models, :foreign_key => :equipment_model_id
   has_many :accessories, :through => :accessories_equipment_models
-  
+
   validates_presence_of :name
   validates_uniqueness_of :name
   validates_presence_of :description
   validates_numericality_of :late_fee
   validates_numericality_of :replacement_fee
   validates_numericality_of :max_per_user, :allow_nil => true
-  
+
   attr_accessible :name, :category_id, :description, :late_fee, :replacement_fee, :max_per_user, :document_attributes, :accessory_ids, :checkout_procedures, :checkin_procedures
-  
+
   #inherits from category if not defined
   def maximum_per_user
     max_per_user || category.maximum_per_user
   end
-  
+
   def self.select_options
     self.find(:all, :order => 'name ASC').collect{|item| [item.name, item.id]}
   end
-  
+
   def document_attributes=(document_attributes)
     document_attributes.each do |attributes|
       documents.build(attributes)
     end
   end
-  
+
   def formatted_description
     lines = self.description.split(/^/)
-    
+
     nice_content = "<p>"
     lines.each do |line|
       if line.include? "<table>" or line.include? "<td>"
         nice_content += line
       else
-        nice_content += line + "<br />" 
+        nice_content += line + "<br />"
       end
     end
     nice_content += "</p>"
   end
-  
+
   def photos
     self.documents.images
   end
-  
+
   def available?(date_range)
     overall_count = self.equipment_objects.size
     date_range.each do |date|
@@ -63,19 +63,20 @@ class EquipmentModel < ActiveRecord::Base
     end
     overall_count
   end
-  
+
   def available_count(date)
     # get the total number of objects of this kind
     # then subtract the total quantity currently checked out, reserved, or overdue
     # TODO: the system does not account for early checkouts.
-    
-    reserved_count = Reservation.find(:all, :conditions => ["checked_in IS NULL and equipment_model_id = ? and start_date <= ? and due_date >= ?", self.id, date.to_time.utc, date.to_time.utc]).size
-    overdue_count = Reservation.find(:all, :conditions => ["checked_in IS NULL and equipment_model_id = ? and due_date <= ?", self.id, Date.today.to_time.utc]).size
-    
+
+    reserved_count = Reservation.find(:all, :conditions => ["checked_in IS NULL and checked_out IS NULL and equipment_model_id = ? and start_date <= ? and due_date >= ?", self.id, date.to_time.utc, date.to_time.utc]).size
+    overdue_count = Reservation.find(:all, :conditions => ["checked_in IS NULL and checked_out IS NOT NULL and equipment_model_id = ? and due_date <= ?", self.id, Date.today.to_time.utc]).size
+
     self.equipment_objects.count - reserved_count - overdue_count
   end
-  
+
   def available_object_select_options
     self.equipment_objects.select{|e| e.available?}.sort_by(&:name).collect{|item| "<option value=#{item.id}>#{item.name}</option>"}
   end
 end
+
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -5,7 +5,7 @@ class Reservation < ActiveRecord::Base
   belongs_to :reserver, :class_name => 'User'
   belongs_to :checkout_handler, :class_name => 'User'
   belongs_to :checkin_handler, :class_name => 'User'
-  
+
   validates_presence_of :reserver
   validates_presence_of :start_date
   validates_presence_of :due_date
@@ -13,15 +13,15 @@ class Reservation < ActiveRecord::Base
   #Currently this prevents checking in overdue items. We can work on a better fix.
   #validate :not_in_past
   validate :start_date_before_due_date
-  
+
   named_scope :pending, {:conditions => ["checked_out IS NULL and checked_in IS NULL"], :order => 'start_date ASC'}
   named_scope :checked_out, lambda { {:conditions => ["checked_out IS NOT NULL and checked_in IS NULL and due_date >=  ?", Time.now.midnight.utc ], :order => 'start_date ASC' } }
   named_scope :overdue, lambda { {:conditions => ["checked_out IS NOT NULL and checked_in IS NULL and due_date < ?", Time.now.midnight.utc ], :order => 'start_date ASC' } }
   named_scope :active, :conditions => ["checked_in IS NULL"] #anything that's been reserved but not returned (i.e. pending, checked out, or overdue)
   named_scope :returned, :conditions => ["checked_in IS NOT NULL and checked_out IS NOT NULL"]
-  
+
   attr_accessible :reserver, :reserver_id, :checkout_handler, :checkout_handler_id, :checkin_handler, :checkin_handler_id, :start_date, :due_date, :checked_out, :checked_in, :equipment_model_id, :equipment_object_id
-  
+
   def status
     #TODO: check this logic
     if checked_out.nil?
@@ -32,23 +32,23 @@ class Reservation < ActiveRecord::Base
       "returned"
     end
   end
-  
+  # These three methods are not being implemented...
   def not_empty
     errors.add_to_base("A reservation must contain at least one item.") if self.equipment_model.nil?
   end
-  
+
   def not_in_past
     errors.add_to_base("A reservation cannot be made in the past!") if self.due_date < Time.now.midnight
   end
-  
+
   def start_date_before_due_date
     errors.add_to_base("A reservation's due date must come after its start date.") if self.due_date < self.start_date
   end
-  
+
   def late_fee
     self.equipment_model.late_fee.to_f
   end
-  
+
   def equipment_list
     raw_text = ""
     self.equipment_models_reservations.each do |equipment_models_reservations|
@@ -60,10 +60,11 @@ class Reservation < ActiveRecord::Base
     end
     raw_text
   end
-  
+
   # def equipment_object_id=(ids)
   #   ids.each do |id|
   #     equipment_objects << EquipmentObject.find(id)
   #   end
   # end
 end
+
