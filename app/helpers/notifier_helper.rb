e8b32e0a60437c91f1d62877d8aa36d4d3789adc
Adam Bray <adam.bray@yale.edu>
2012-05-28 20:18:58 -0400

    fixed more view errors (html escaping and content_for errors)

--- app/helpers/notifier_helper.rb
+++ app/helpers/notifier_helper.rb
@@ -1,7 +1,12 @@
 module NotifierHelper
   def replace_variables(body)
     return "" if body.nil?
-    body.gsub("@user@", @reservation.reserver.name).gsub("@reservation_id@", @reservation.id.to_s).gsub("@department_name@", Settings.department_name).gsub("@equipment_list@", @reservation.equipment_model.name).gsub("@return_date@", @reservation.due_date.to_date.to_s(:long)).gsub("@late_fee@", number_to_currency(@reservation.late_fee))
+    body.gsub("@user@", @reservation.reserver.name)
+    body.gsub("@reservation_id@", @reservation.id.to_s)
+    body.gsub("@department_name@", Settings.department_name)
+    body.gsub("@equipment_list@", @reservation.equipment_model.name)
+    body.gsub("@return_date@", @reservation.due_date.to_date.to_s(:long))
+    body.gsub("@late_fee@", number_to_currency(@reservation.late_fee))
   end
 end
 
