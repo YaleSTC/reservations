f3282ba811fdae0f2524fb021d360b2f978dd1b9
Nick Maas <ncmaas@gmail.com>
2012-06-01 16:52:39 -0400

    Cleaned up the mess I made, by deleting all these unncesesary files

--- db/migrate/20110103203259_add_active_to_equipment_models.rb
+++
@@ -1,9 +0,0 @@
-class AddActiveToEquipmentModels < ActiveRecord::Migration
-  def self.up
-    add_column :equipment_models, :active, :boolean, :default => true
-  end
-
-  def self.down
-    remove_column :equipment_models, :active
-  end
-end
--- vendor/plugins/rubycas-client/examples/rails/config/routes.rb
+++
@@ -1,4 +0,0 @@
-ActionController::Routing::Routes.draw do |map|
-  map.root :controller => "simple_example"
-  map.connect ':controller/:action/:id'
-end
