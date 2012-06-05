795e37862d55512bcbf5905a3ee358030633276c
Maria Altyeva <ms.altyeva@gmail.com>
2012-06-05 16:29:25 -0400

    fixed gsub error

--- app/helpers/user_mailer_helper.rb
+++ app/helpers/user_mailer_helper.rb
@@ -1,12 +1,12 @@
 module UserMailerHelper
   def replace_variables(body)
     return "" if body.nil?
-    body = body.gsub("@user@", "Maria")
-    body.gsub("@reservation_id@", @reservation.id.to_s)
-    body.gsub("@department_name@", Settings.department_name)
-    body.gsub("@equipment_list@", @reservation.equipment_model.name)
-    body.gsub("@return_date@", @reservation.due_date.to_date.to_s(:long))
-    body.gsub("@late_fee@", number_to_currency(@reservation.late_fee))
+    body = body.gsub("@user@", @reservation.reserver.name)
+    body = body.gsub("@reservation_id@", @reservation.id.to_s)
+    body = body.gsub("@department_name@", Settings.department_name)
+    body = body.gsub("@equipment_list@", @reservation.equipment_model.name)
+    body = body.gsub("@return_date@", @reservation.due_date.to_date.to_s(:long))
+    body = body.gsub("@late_fee@", number_to_currency(@reservation.late_fee))
   end
 end
 
