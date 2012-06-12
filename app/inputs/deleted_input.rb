d035b9d44b4819bdcda1b63839c7d111921f9960
Ian Suvak <ian.suvak@yale.edu>
2012-06-12 16:54:17 -0400

    implemented deleted input for simple_form

---
+++ app/inputs/deleted_input.rb
@@ -0,0 +1,10 @@
+class DeletedInput < SimpleForm::Inputs::BooleanInput
+  def input
+    build_check_box
+  end
+  
+  def build_check_box(unchecked_value = "")
+    @builder.check_box(attribute_name,  (@builder.object.deleted_at.blank? ? {:checked => false} : {:checked => true}), Time.now, "")
+  end
+end
+  
