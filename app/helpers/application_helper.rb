f0ed76d668340a77423e776bf1624ab71cc78e66
Nick Maas <ncmaas@gmail.com>
2012-06-12 11:42:11 -0400

    Succesfully implemented chain-deletion and chain-reactivation for Categories, EMs, and EOs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,21 +1,30 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
 
-  def deactivateX(a)
-      @objects_class = a.class.name.constantize #Find out the class of the object we are going to deactivate
-        @all_associations = @objects_class.reflect_on_all_associations(:has_many).collect(&:name).each do |x| #For every child object of the parent:
-            if (x == :equipment_models)
-               EquipmentModel.not_deleted.where(:category_id => a.id).each do |y| #Deactivate any Equipment Models that belong to a particular category
-                  deactivateX(y)
+  def deactivateChildren(currentModel)
+      @objects_class = currentModel.class.name.constantize #Find out the class of the object we are going to deactivate
+        @all_associations = @objects_class.reflect_on_all_associations(:has_many).collect(&:name).each do |association| #For every child object of the parent:
+            if (association == :equipment_models) #Deactivate any Equipment Models associated with currentModel
+               EquipmentModel.not_deleted.where(:category_id => currentModel.id).each do |child| 
+                  deactivateChildren(child)
                end
             end
-            if x == :equipment_objects
-               EquipmentObject.not_deleted.where(:equipment_model_id => a.id).each do |y| #Deactivate any Equipment Objects that belong to a particular Equipment Model
-                  deactivateX(y)
+            if association == :equipment_objects #Deactivate any Equipment Objects associated with currentModel
+               EquipmentObject.not_deleted.where(:equipment_model_id => currentModel.id).each do |child| 
+                  deactivateChildren(child)
                end
             end
          end
-      a.destroy
+      currentModel.destroy
+  end
+
+  def activateParents(currentModel)
+    if (currentModel.class == EquipmentObject) #Equipment Objects have EMs and Categories that may need to be reactivated
+      EquipmentModel.find(currentModel.equipment_model_id).revive #Reactivate the EM
+      Category.find(EquipmentModel.find(currentModel.equipment_model_id).category_id).revive #Reactivate the Category
+    elsif (currentModel.class == EquipmentModel) #EMs have Categories that may need to be reactivated
+      Category.find(currentModel.category_id).revive #Reactivate the category
+    end
   end
 
 end
