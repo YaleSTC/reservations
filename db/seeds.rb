6619bb5e120c17e44e73d8ff362f1197e3b6807a
Machiste Quintana <machiste.quintana@yale.edu>
2012-06-18 15:06:52 -0400

    seed script now generates checkinprocedures, checkoutprocedures and reservations! #58

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,13 @@
 # As it stands, you'll need to follow the format and create a new seed generation block for each
 # model you want to seed in the database, every time you create a model that isn't already here.
 # Right now, it only generates Users, Categories, EquipmentModels, and EquipmentObjects.
+# Order matters!! The script will fail if certain records (Catgeories, EquipmentModels) aren't
+# generated first and in the order listed.
 
 require 'ffaker'
+
+
+#-------METHODS
 
 #Method for prompting the user for the number of records per model they want to seed into the database.
 def ask_for_records( model )
@@ -13,9 +18,15 @@ def ask_for_records( model )
   STDIN.gets.chomp.to_i
 end
 
+def time_rand from = 0.0, to = Time.now
+  Time.at(from + rand * (to.to_f - from.to_f))
+end
+
 
 #Random object that is used throughout for generating fake data that FFaker can't
 r = Random.new
+
+#Start script
 
 if User.all.empty?
   STDOUT.puts "ERROR: You must sign into the app via CAS first to create a superuser account for your netID."
@@ -23,8 +34,8 @@ else
   #User generation
   entered_num = ask_for_records("User")
 
-  if entered_num.integer? && entered_num >= 0
-    entered_num.times do
+  if entered_num.integer? && entered_num > 0
+    user = entered_num.times.map do
       User.create! do |u|
         u.first_name = Faker::Name.first_name
         u.last_name = Faker::Name.last_name
@@ -33,11 +44,14 @@ else
         u.email = Faker::Internet.email
         u.login = (0...3).map{65.+(rand(25)).chr}.join.downcase + r.rand(2..99).to_s
         u.affiliation = "YC " + ["BK", "BR", "CC", "DC", "ES", "JE", "MC", "PC", "SM", "SY", "TC", "TD"].sample + " " + r.rand(2012..2015).to_s
+        u.adminmode = false
+        u.is_checkout_person = [true, false].sample
       end
     end
+    user[0].is_checkout_person = true
     STDOUT.puts "#{entered_num} records successfully created!"
   else
-    STDOUT.puts "Please enter a whole number."
+    STDOUT.puts "Please enter a whole number greater than 0."
     entered_num = STDIN.gets.chomp.to_i
   end
 
@@ -68,13 +82,13 @@ else
     equipment_model = entered_num.times.map do
       EquipmentModel.create! do |em|
         em.name = Faker::Product.product + " " + r.rand(1..9001).to_s
-        em.description = Faker::Lorem.paragraph(4)
+        em.description = Faker::HipsterIpsum.paragraph(4)
         em.late_fee = r.rand(50.00..1000.00).round(2).to_d
         em.replacement_fee = r.rand(50.00..1000.00).round(2).to_d
         em.max_per_user = r.rand(1..40)
         em.active = true
-        #em.checkout_procedures = Faker::Lorem.sentences(4)
-        #em.checkin_procedures = Faker::Lorem.sentences(4)
+        #em.checkout_procedures = Faker::HipsterIpsum.sentences(4)
+        #em.checkin_procedures = Faker::HipsterIpsum.sentences(4)
         em.category_id = category.flatten[r.rand(0...category.length)].id
       end
     end
@@ -88,18 +102,79 @@ else
   #EquipmentObject generation
   entered_num = ask_for_records("EquipmentObject")
 
-  if entered_num.integer? && entered_num >= 0
-    entered_num.times do
+  if entered_num.integer? && entered_num > 0
+    equipment_object = entered_num.times.map do
       EquipmentObject.create! do |eo|
         eo.name = "Number #{(0...3).map{65.+(rand(25)).chr}.join}" + r.rand(1..9001).to_s
         eo.serial = (0...8).map{65.+(rand(25)).chr}.join
         eo.active = true
-        eo.equipment_model_id = equipment_model.flatten[r.rand(0...equipment_model.length)].id
+        eo.equipment_model_id = equipment_model.flatten.sample.id
       end
     end
     STDOUT.puts "#{entered_num} records successfully created!"
   else
-    STDOUT.puts "Please enter a whole number."
+    STDOUT.puts "Please enter a whole number greater than 0."
     entered_num = STDIN.gets.chomp.to_i
   end
+
+  #CheckinProcedure generation
+  entered_num = ask_for_records("CheckinProcedure")
+
+  if entered_num.integer? && entered_num > 0
+    checkin_procedure = entered_num.times.map do
+      CheckinProcedure.create! do |chi|
+        chi.step = Faker::HipsterIpsum.sentence
+        chi.equipment_model_id = equipment_model.flatten.sample.id
+      end
+    end
+    STDOUT.puts "#{entered_num} records successfully created!"
+  else
+    STDOUT.puts "Please enter a whole number greater than 0."
+    entered_num = STDIN.gets.chomp.to_i
+  end
+  
+  #CheckoutProcedure generation
+  entered_num = ask_for_records("CheckoutProcedure")
+
+  if entered_num.integer? && entered_num > 0
+    checkin_procedure = entered_num.times.map do
+      CheckinProcedure.create! do |cho|
+        cho.step = Faker::HipsterIpsum.sentence
+        cho.equipment_model_id = equipment_model.flatten.sample.id
+      end
+    end
+    STDOUT.puts "#{entered_num} records successfully created!"
+  else
+    STDOUT.puts "Please enter a whole number greater than 0."
+    entered_num = STDIN.gets.chomp.to_i
+  end
+  
+  #Reservation generation
+  entered_num = ask_for_records("Reservation")
+  
+  if entered_num.integer? && entered_num > 0
+    reservation = entered_num.times.map do
+      random_time = time_rand Time.local(2010, 1, 1)
+      random_due_date = time_rand(random_time, Time.now.next_week)
+
+      Reservation.create! do |res|       
+        res.reserver_id = user.flatten.sample.id
+        res.checkout_handler_id = user.flatten.select{|usr| usr.is_checkout_person}.sample.id
+        res.checkin_handler_id = user.flatten.select{|usr| usr.is_checkout_person}.sample.id
+        res.start_date = random_time.to_datetime
+        res.due_date = [random_due_date.to_datetime, (random_time + category.flatten.sample.max_checkout_length.days).to_datetime].sample
+        res.checked_out = [nil, random_time.to_datetime].sample
+        res.checked_in = [nil, random_due_date.to_datetime, time_rand(random_due_date, random_due_date.next_month).to_datetime].sample
+        res.equipment_model_id = equipment_object.flatten.sample.equipment_model_id
+        res.equipment_object_id = equipment_object.flatten.sample.id
+        res.notes = Faker::HipsterIpsum.paragraph(4)
+        res.notes_unsent = [true, false].sample
+      end
+    end
+    STDOUT.puts "#{entered_num} records successfully created!"
+  else
+    STDOUT.puts "Please enter a whole number greater than 0."
+    entered_num = STDIN.gets.chomp.to_i
+  end
+  
 end
