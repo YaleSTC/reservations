96653075fee6ce003ab4ded7df9336db70b82275
Maria Altyeva <ms.altyeva@gmail.com>
2012-06-19 12:18:24 -0400

    Merge conflicts

--- lib/development_mail_interceptor.rb
+++ lib/development_mail_interceptor.rb
@@ -1,10 +1,6 @@
 class DevelopmentMailInterceptor
   def self.delivering_email(message)
     message.subject = "[#{message.to}] #{message.subject}"
-<<<<<<< HEAD
-    message.to = "stc_mail_testing@googlegroupc.com"
-=======
     message.to = "stc_mail_testing@googlegroups.com"
->>>>>>> rails3_errors
   end
 end
