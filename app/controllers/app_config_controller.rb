71ea9bd46ca7cc3927367bea3e65ad8ab419e0e2
Machiste Quintana <machiste.quintana@yale.edu>
2012-06-19 21:15:37 -0400

    added some logic to app config to validate presence of site title #82

--- app/controllers/app_config_controller.rb
+++ app/controllers/app_config_controller.rb
@@ -6,10 +6,15 @@ class AppConfigController < ApplicationController
 
   def update
     #update each Setting (field names are the same as Settings attribute names)
-    params.each do |key, value|
-      Settings.send(key+'=', value)
+    unless params[:site_title].blank?
+      params.each do |key, value|
+        Settings.send(key+'=', value)
+      end
+      flash[:notice] = "Application settings updated successfully."
+      redirect_to root_path
+    else
+      flash[:error] = "Site title can't be blank!"
+      redirect_to edit_app_config_path
     end
-    flash[:notice] = "Application settings updated successfully."
-    redirect_to root_path
   end
 end
