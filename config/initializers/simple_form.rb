fbd438e0738cded1860b1cdbf97cc236f7bea0c8
Machiste Quintana <machiste.quintana@yale.edu>
2012-06-25 13:59:53 -0400

    updated simple_form config to use btn-primary as its default button class #109

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -95,7 +95,7 @@ SimpleForm.setup do |config|
   config.boolean_style = :nested
 
   # Default class for buttons
-  config.button_class = 'btn'
+  config.button_class = 'btn btn-primary'
 
   # Method used to tidy up errors. Specify any Rails Array method.
   # :first lists the first message for each field.
