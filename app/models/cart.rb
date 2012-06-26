05f8f449613119e8c49f3ec2c2036452a2268cce
noreaster <nw.northeaster345@gmail.com>
2012-06-26 13:01:06 -0400

    fixed comments and validations

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -113,14 +113,17 @@ class Cart
     end
   end
   
-  #Check if the user should renew models
+  #Create an array of all the reservations that should be renewed instead of having a new reservation
   def renewable_reservations
     user_reservations = reserver.reservations
     renewable_reservations = []
     @items.each do |item|
       cart_item_count = item.quantity #renew up to this many of the item
       matching_reservations = user_reservations.each do |res|
-        if (res.due_date.to_date == @start_date &&
+        # the end date should be the same as the start date
+        # the reservation should be renewable
+        # also the user should only renew as many reservations as they have in their cart
+        if (res.due_date.to_date == @start_date &&                
            res.equipment_model_id == item.equipment_model_id &&
            cart_item_count > 0 &&
            res.is_eligible_for_renew?)
@@ -198,8 +201,9 @@ class Cart
   end
 
   ## Item validations
-  #Check if the user should renew their 
-  def no_renewable_models
+  # Check if the user should renew their existing reservation rather than creating a new one
+  def no_renewable_models?
+    none_to_renew = true
     reservations_to_renew = renewable_reservations
     
     eq_model_names = reservations_to_renew.collect {|r| EquipmentModel.find(r.equipment_model_id).name}.uniq
@@ -207,8 +211,10 @@ class Cart
     unless reservations_to_renew.empty?
       eq_model_names.each do |eq_model_name|
         errors.add(:items, reserver.name +" should renew " + eq_model_name)
+        none_to_renew = false
       end
     end
+    return none_to_renew
   end
   
   #Check that the reserver does not exceeds the maximum number of any equipment models
