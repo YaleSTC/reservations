9eeb51f4fd60cdeb41a9b2f85672097485f1ef86
noreaster <nw.northeaster345@gmail.com>
2012-06-28 14:12:34 -0400

    Added equipment model counts

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -7,16 +7,48 @@ class ReportsController < ApplicationController
     users = [current_user]
     
     # res_set = Reservation.starts_on_days(@start_date,@end_date).reserver_is_in(users)
-    res_set = Reservation.starts_on_days(@start_date,@end_date)
-     
-    res_counts = {}
-    res_counts[:reserved]    = res_set.reserved.count
-    res_counts[:checked_out] = res_set.checked_out.count
-    res_counts[:overdue]     = res_set.overdue.count
-    res_counts[:returned]    = res_set.returned.count
-    res_counts[:missed]      = res_set.missed.count
-    res_counts[:upcoming]    = res_set.upcoming.count
-    @res_counts = res_counts
+    full_set = Reservation.starts_on_days(@start_date,@end_date).includes(:equipment_model)
+    @res_stat_sets = {}
+    
+    # reservation relations for each of the scopes
+    res_rels = {}
+    res_rels[:total]        = full_set
+    res_rels[:reserved]     = full_set.reserved
+    res_rels[:checked_out]  = full_set.checked_out
+    res_rels[:overdue]      = full_set.overdue
+    res_rels[:returned]     = full_set.returned
+    res_rels[:missed]       = full_set.missed
+    res_rels[:upcoming]     = full_set.upcoming
+    
+    res_sets = [:all_models]
+    # binding.pry
+    # should this be redone?  Mostly done in two parts to only collect the uniq ids
+    eq_model_ids = [nil]
+    eq_model_ids += full_set.collect {|res| res.equipment_model_id}.uniq 
+    
+    eq_model_names = {}
+    EquipmentModel.find(eq_model_ids).each do |em|
+      eq_model_names[em.id] = em.name.to_sym
+    end
+    
+    
+    eq_model_ids.each do |model_id|
+      res_counts = {}
+      res_rels.each do |key, rel|
+        if model_id
+          res_counts[key] = rel.where(:equipment_model_id => model_id).count
+        else
+          res_counts[key] = rel.count
+        end
+      end
+      if model_id
+        @res_stat_sets[eq_model_names[model_id]] = res_counts
+      else
+        @res_stat_sets[:"All Models"] = res_counts
+      end
+    end
+    
+    
     # scopes.each do |scope|
     #       res_counts[scope] = res_set.method(scope).call.count
     #     end
