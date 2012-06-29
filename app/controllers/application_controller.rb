468fc3d8105c320ad59ca322034118a07cc39b7b
Ian Suvak <ian.suvak@yale.edu>
2012-06-29 10:46:25 -0400

    remove extraneous code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,6 @@ class ApplicationController < ActionController::Base
   before_filter :set_view_mode
   before_filter :current_user
   #before_filter :bind_pry_before_everything
-  before_filter :cache_users
 
   helper_method :current_user
   helper_method :cart
@@ -77,10 +76,6 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def cache_users
-    @@users_array ||= User.select("first_name, last_name, login, id, deleted_at").reject { |user| ! user.deleted_at.nil?}
-    @@users_array
-  end
   #-------- end before_filter methods --------
 
   def update_cart
