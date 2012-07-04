266ea06251c9bdd4a1a74b53ba2069d8ec078ac5
Machiste Quintana <machiste.quintana@yale.edu>
2012-07-04 12:47:21 -0400

    #135 even more progress on layout

--- app/helpers/equipment_models_helper.rb
+++ app/helpers/equipment_models_helper.rb
@@ -1,2 +1,6 @@
 module EquipmentModelsHelper
+  def bar_progress
+    width = ((@equipment_model.available?(cart.start_date..cart.due_date) || "0").to_i) / @equipment_model.equipment_objects.size
+    @width_percentage = number_to_percentage(width * 100, :precision => 0)
+  end
 end
