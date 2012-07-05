01154ec5a63f57d6848cd0e270df8df0bd659d9a
noreaster <nw.northeaster345@gmail.com>
2012-07-05 15:28:16 -0400

    Subreport links, users info, some formatting

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,7 +1,8 @@
 class ReportsController < ApplicationController
   # before_filter :require_admin
-  ResRelation = Struct.new(:name, :relation, :params)
-  StatRow = Struct.new(:name, :link_info, :data)
+  ResRelation = Struct.new(:name, :relation, :params) # relations to build data columns
+  StatRow = Struct.new(:name, :data, :link_path) # output structure
+  ResSetInfo = Struct.new(:name, :id_type, :ids, :link_path) #info for a reservation set
   
   def index
     @res_stat_sets = []
@@ -19,29 +20,35 @@ class ReportsController < ApplicationController
     # should this be redone?  Mostly done in two parts to only collect the uniq ids
     eq_model_ids = full_set.collect {|res| res.equipment_model_id}.uniq 
     eq_models = EquipmentModel.includes(:category).find(eq_model_ids)
-
-    eq_model_names = {}
-    category_names = {}
-
+    
+    eq_model_names = []
+    category_names = []
+    
+    cat_info = {}
     eq_models.each do |em|
-      eq_model_names[em.name.to_sym] = [em.id]
-      cat_name = em.category.name.to_sym
-      if category_names[cat_name]
-        category_names[cat_name] << em.id
+      eq_model_names << ResSetInfo.new(em.name, :equipment_model_id, [em.id],for_model_report_path(:id => em.id))
+      if cat_info[em.category.id]
+        cat_info[em.category.id] << em.id
       else
-        category_names[cat_name] = [em.id]
+        cat_info[em.category.id] = [em.id]
       end
+    end
+    categories = Category.find(cat_info.keys)
+    categories.each do |cat|
+      category_names << ResSetInfo.new(cat.name,:equipment_model_id, cat_info[cat.id])
     end
 
     # take all the sets of reservations and get stats on them
     # sets of reservations are passed in by name then models associated
-    res_sets = [{:"All Models" => nil}, category_names, eq_model_names]
+    all_models = [ResSetInfo.new("All Models", :equipment_model_id)]
+    res_sets = [all_models, category_names, eq_model_names]
     @table_col_names = res_rels.collect{|r| r[:name]} # + [:"User Counts"]
-    res_sets.each do |name_hash|
-      @res_stat_sets << collect_stat_set(name_hash,res_rels)
+    res_sets.each do |info_struct|
+      @res_stat_sets << collect_stat_set(info_struct,res_rels)
     end
   end
 
+  #sub report for a particular model
   def for_model
     res_set = Reservation.includes(:equipment_model,:equipment_object).where(:equipment_model_id => params[:id])
     res_rels = default_relations(res_set)
@@ -49,23 +56,28 @@ class ReportsController < ApplicationController
     @table_col_names = res_rels.collect{|r| r[:name]}
     
     eq_model = EquipmentModel.find(params[:id])
-    name_hash = {eq_model.name.to_sym => [eq_model.id]}
-    @stat_set = collect_stat_set(name_hash,res_rels)
-
-    obj_rels = res_rels.each do |r|
-      r.params[:id_type] = :equipment_object_id
+    eq_info = [ResSetInfo.new(eq_model.name,:equipment_model_id, [eq_model.id])]
+    @stat_set = collect_stat_set(eq_info,res_rels)
+    
+    user_ids = res_set.collect{|r| r.reserver_id}.uniq
+    users = User.find(user_ids)
+    user_info = []
+    users.each do |user|
+      user_info << ResSetInfo.new(user.name,:reserver_id, [user.id])
     end
+    user_set = collect_stat_set(user_info,res_rels)
+    
     eq_objects = EquipmentObject.find(:all, :conditions => {:equipment_model_id => eq_model.id})
-    obj_hash = {}
+    obj_info = []
     eq_objects.each do |obj|
-      obj_name = obj.name.to_sym 
-      obj_hash[obj_name] = [obj.id]
+      obj_info << ResSetInfo.new(obj.name,:equipment_object_id, [obj.id])
     end
-    @obj_set = collect_stat_set(obj_hash,obj_rels)
+    obj_set = collect_stat_set(obj_info,res_rels)
+    @model_tables = {:equipment_model => @stat_set,:equipment_objects => obj_set, :users => user_set}
   end
 
   private
-  def default_relations(res_set,*defaults)
+  def default_relations(res_set,*conditions)
     # reservation relations for each of the scopes
     res_rels = []
     res_rels << ResRelation.new("Total", res_set)
@@ -75,7 +87,8 @@ class ReportsController < ApplicationController
     res_rels << ResRelation.new("Reserved", res_set.returned)
     res_rels << ResRelation.new("Missed", res_set.missed)
     res_rels << ResRelation.new("Upcoming", res_set.upcoming)
-    defaults = defaults.empty? ? {:id_type => :equipment_model_id, :stat_type => :count} : defaults.first 
+    defaults = {:stat_type => :count}
+    defaults.merge!(conditions.first) unless conditions.empty?
     res_rels.each do |r|
       if r.params
         r.params = defaults
@@ -87,26 +100,26 @@ class ReportsController < ApplicationController
     return res_rels
   end
 
-  def collect_stat_set(name_hash,res_rels)
+  def collect_stat_set(info_struct,res_rels)
     stat_set = []
-    name_hash.each do |name, ids|
-      stat_row = [name]
+    info_struct.each do |info|
+      stat_row = StatRow.new(info.name,[],info.link_path)
       res_rels.each do |rel_struct|
         rel = rel_struct[:relation]
         params = rel_struct[:params]
         # select the reservations whose id of :id_type that are in the array of ids 
         # e.g. equipment_model_id is a member of [1,2,3]
-        res_set = ids ? rel.select{|res| ids.include?(res.send(params[:id_type]))} : rel.all
+        res_set = info.ids ? rel.select{|res| info.ids.include?(res.send(info.id_type))} : rel.all
         case params[:stat_type]
         when :count
           if params[:secondary_id]
-            stat_row << res_set.collect{|r| r.send(params[:secondary_id])}.uniq.count
+            stat_row.data << res_set.collect{|r| r.send(params[:secondary_id])}.uniq.count
           else
-            stat_row << res_set.count
+            stat_row.data << res_set.count
           end
         when :duration
           durations = res_set.collect{|res| res.due_date.to_date - res.start_date.to_date}
-          stat_row << (durations.inject{|sum,k| sum + k}.to_f / durations.count).round(2) #avg planned duration
+          stat_row.data << (durations.inject{|sum,k| sum + k}.to_f / durations.count).round(2) #avg planned duration
         end
       end
       stat_set << stat_row
