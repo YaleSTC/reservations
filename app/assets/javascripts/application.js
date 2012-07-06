1edfb6597f498f6a8280a2af106bd7582a12aa09
Machiste Quintana <machiste.quintana@yale.edu>
2012-07-06 17:19:35 -0400

    added animation to progress bar #135

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -61,6 +61,23 @@ $(document).ready(function() {
 	});
 
 	$(".btn#modal").tooltip();
+	
+  $('.progress .bar').each(function() {
+      var me = $(this);
+      var perc = me.attr("data-percentage");
+      var current_perc = 0;
+
+      var progress = setInterval(function() {
+          if (current_perc>=perc) {
+              clearInterval(progress);
+          } else {
+              current_perc = perc;
+              me.css('width', (current_perc)+'%');
+          }
+
+      }, 100);
+
+  });
 
 });
 // to disable selection of dates in the past with datepicker
