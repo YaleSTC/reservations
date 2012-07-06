c0fa55b157784d554c728b58f3267031dc9aa9de
Machiste Quintana <machiste.quintana@yale.edu>
2012-07-06 15:42:04 -0400

    fixed progress bar helpers #135

--- app/helpers/equipment_models_helper.rb
+++ app/helpers/equipment_models_helper.rb
@@ -1,21 +1,27 @@
 module EquipmentModelsHelper
   def bar_progress
-    width = @equipment_model.equipment_objects.size != 0 ? (((@equipment_model.available?(cart.start_date..cart.due_date) || "0").to_i) / 
-    @equipment_model.equipment_objects.size) * 100 : 0
-    @width_percentage = number_to_percentage(width || "0%", :precision => 0)
+    define_width
+    @width_percentage = number_to_percentage(@width * 100 || "0%", :precision => 0)
   end
   
   def progress_color
-    width = @equipment_model.equipment_objects.size != 0 ? (((@equipment_model.available?(cart.start_date..cart.due_date) || "0").to_i) / 
-    @equipment_model.equipment_objects.size) * 100 : 0
-    a_third = (100 / 3)
-    
-    if width > (a_third * 2)
+    define_width  
+    if @width > (1/3 * 2)
       @color = "progress-success"
-    elsif width > a_third
+    elsif @width > 1/3
       @color = "progress-warning"
-    elsif width < a_third
+    elsif @width < 1/3
       @color = "progress-danger"
+    end
+  end
+  
+  private
+  
+  def define_width
+    if @equipment_model.equipment_objects.size > 0
+      @width = (@equipment_model.available?(cart.start_date..cart.due_date).to_f) / @equipment_model.equipment_objects.size.to_f     
+    else
+      @width = 0
     end
   end
 end
