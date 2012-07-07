d2e57aabc4b115a9153577a51042521829666414
Machiste Quintana <machiste.quintana@yale.edu>
2012-07-07 16:52:28 -0400

    #135 more tweaks to the subnav bar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -79,36 +79,37 @@ $(document).ready(function() {
       }, 100);
   });
 
-$('.associated_em_box img').popover({ placement: 'bottom' });
+	$('.associated_em_box img').popover({ placement: 'bottom' });
 
-$('#subnavbar').scrollspy();
-// fix sub nav on scroll
-var $win = $(window)
-  , $nav = $('.subnav')
-  , navTop = $('.subnav').length && $('.subnav').offset().top - 40
-  , isFixed = 0
-	, $hiddenName = $('.subnav .hide')
+	$('#subnavbar').scrollspy();
+	// fix sub nav on scroll
+	var $win = $(window)
+	  , $nav = $('.subnav')
+	  , navTop = $('.subnav').length && $('.subnav').offset().top - 40
+	  , isFixed = 0
+		, $hiddenName = $('.subnav .hide')
 
-processScroll()
+	processScroll()
 
-// hack sad times - holdover until rewrite for 2.1
-$nav.on('click', function () {
-  if (!isFixed) setTimeout(function () {  $win.scrollTop($win.scrollTop() - 47) }, 10)
-})
+	// hack sad times - holdover until rewrite for 2.1
+	$nav.on('click', function () {
+	  if (!isFixed) setTimeout(function () {  $win.scrollTop($win.scrollTop() - 47) }, 10)
+	})
 
-$win.on('scroll', processScroll)
+	$win.on('scroll', processScroll)
 
-function processScroll() {
-  var i, scrollTop = $win.scrollTop()
-  if (scrollTop >= navTop && !isFixed) {
-    isFixed = 1
-    $nav.addClass('subnav-fixed')
-  } else if (scrollTop <= navTop && isFixed) {
-    isFixed = 0
-    $nav.removeClass('subnav-fixed')
-		$hiddenName.addClass('hide')
-  }
-}
+	function processScroll() {
+	  var i, scrollTop = $win.scrollTop()
+	  if (scrollTop >= navTop && !isFixed) {
+	    isFixed = 1
+	    $nav.addClass('subnav-fixed')
+			$hiddenName.removeClass('hide')
+	  } else if (scrollTop <= navTop && isFixed) {
+	    isFixed = 0
+	    $nav.removeClass('subnav-fixed')
+			$hiddenName.addClass('hide')
+	  }
+	}
 
 });
 // to disable selection of dates in the past with datepicker
