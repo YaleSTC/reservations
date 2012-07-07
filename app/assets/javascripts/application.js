6ddac5400f90591922ddb776f3f68fd4d31bb580
Machiste Quintana <machiste.quintana@yale.edu>
2012-07-07 16:05:35 -0400

    added scrollspy and responsive #135

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -81,6 +81,35 @@ $(document).ready(function() {
 
 $('.associated_em_box img').popover({ placement: 'bottom' });
 
+$('#subnavbar').scrollspy();
+// fix sub nav on scroll
+var $win = $(window)
+  , $nav = $('.subnav')
+  , navTop = $('.subnav').length && $('.subnav').offset().top - 40
+  , isFixed = 0
+	, $hiddenName = $('.subnav .hide')
+
+processScroll()
+
+// hack sad times - holdover until rewrite for 2.1
+$nav.on('click', function () {
+  if (!isFixed) setTimeout(function () {  $win.scrollTop($win.scrollTop() - 47) }, 10)
+})
+
+$win.on('scroll', processScroll)
+
+function processScroll() {
+  var i, scrollTop = $win.scrollTop()
+  if (scrollTop >= navTop && !isFixed) {
+    isFixed = 1
+    $nav.addClass('subnav-fixed')
+  } else if (scrollTop <= navTop && isFixed) {
+    isFixed = 0
+    $nav.removeClass('subnav-fixed')
+		$hiddenName.addClass('hide')
+  }
+}
+
 });
 // to disable selection of dates in the past with datepicker
 $.datepicker.setDefaults({
