df3ed97207fa5384d1c4df7a84740ca3844be77b
Machiste Quintana <machiste.quintana@yale.edu>
2012-07-09 12:46:38 -0400

    #135 implemented associated equipment models on show page

--- app/helpers/equipment_models_helper.rb
+++ app/helpers/equipment_models_helper.rb
@@ -15,6 +15,14 @@ module EquipmentModelsHelper
     end
   end
   
+  def evaluate_img_presence equipment_model
+    if equipment_model.photo.exists?
+      equipment_model.photo.url(:small)
+    else
+      "no-image-260.gif"
+    end
+  end
+  
   private
   
   def define_width
