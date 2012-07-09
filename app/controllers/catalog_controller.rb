b5831f1396b88f4822bb470d2242940aef7d8f57
David Goerger <david.goerger@yale.edu>
2012-07-09 15:12:57 -0400

    implements #153, still needs styling and pagination

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -29,29 +29,6 @@ class CatalogController < ApplicationController
     flash[:notice] = "Invalid equipment_model"
     redirect_to root_path
   end
-
-# quicksearch is gone; delete after 10 july 2012 if no issues
-#  def search
-#    if params[:category].nil?
-#      redirect_to catalog_path
-#    else
-#      #update dates
-#      session[:cart].set_start_date(Date.strptime(params[:cart][:start_date_quicksearch],'%m/%d/%Y')) # quicksearch is gone
-#      session[:cart].set_due_date(Date.strptime(params[:cart][:due_date_quicksearch],'%m/%d/%Y')) # this will likewise never be called
-#      if !cart.valid_dates?
-#        flash[:error] = cart.errors.values.flatten.join("<br/>").html_safe
-#        cart.errors.clear
-#        redirect_to root_path
-#      else
-#        @category = Category.find(params[:category])
-#        @equipment_models = @category.equipment_models.select{|e| e.available?(cart.start_date..cart.due_date)}
-#        @equipment_models_by_category = @equipment_models.sort_by(&:name).group_by(&:category)
-#
-#        flash.now[:notice] = "The following #{@category.name.pluralize} are available from #{cart.start_date} to #{cart.due_date}:"
-#        render :action => :index
-#      end
-#    end
-#  end
   
   def update_user_per_cat_page
     session[:user_per_cat_page] = params[:user_cat_items_per_page] if !params[:user_cat_items_per_page].blank?
@@ -59,6 +36,12 @@ class CatalogController < ApplicationController
       format.html{redirect_to root_path}
       format.js{render :action => "cat_pagination"}
     end
+  end
+
+  def search
+    @equipment_results = EquipmentModel.catalog_search(params[:query])
+    @category_results = Category.catalog_search(params[:query])
+    render 'search_results' and return
   end
   
 end
