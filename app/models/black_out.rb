a56add68aea72d5e0eeba345f87dc4075121c78d
Nick Maas <ncmaas@gmail.com>
2012-07-12 17:13:42 -0400

    date picker streamlined

--- app/models/black_out.rb
+++ app/models/black_out.rb
@@ -3,6 +3,12 @@ class BlackOut < ActiveRecord::Base
   belongs_to :equipment_model
   attr_accessible :start_date, :end_date, :notice, :equipment_model_id, :black_out_type, :created_by
  
+  validates :notice, 
+            :start_date,
+            :equipment_model_id,
+            :black_out_type, 
+            :end_date, :presence => true
+
   def self.date_is_blacked_out(date) #Returns the black_out object that blacks out the day if the day is blacked out. Otherwise, returns nil.
      BlackOut.all.each do |black_out|
        if ((black_out.start_date..black_out.end_date).cover?(date))
