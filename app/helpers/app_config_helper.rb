a93897a4aeb67388c7049cca31342c4f73e03803
Machiste Quintana <machiste.quintana@yale.edu>
2012-07-19 13:38:54 -0400

    generalized paperclip_error_field helper, paperclip errors now display properly in forms

--- app/helpers/app_config_helper.rb
+++ app/helpers/app_config_helper.rb
@@ -5,10 +5,4 @@ module AppConfigHelper
     end
   end
 
-  def paperclip_field_error(local_form_variable, field)
-    # Field must be symbol
-    unless local_form_variable.error(field).blank?
-      'error'
-    end
-  end
 end
