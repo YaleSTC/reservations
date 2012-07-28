e8017130712d9e506ebb7e2c3e7420bb4851df29
Machiste Quintana <machiste.quintana@yale.edu>
2012-07-28 02:37:18 -0400

    cleaned up comments, helperized blackout error code in the catalog

--- app/helpers/black_outs_helper.rb
+++ app/helpers/black_outs_helper.rb
@@ -1,2 +1,16 @@
 module BlackOutsHelper
+  def blackout_errors
+    if (a = BlackOut.date_is_blacked_out(cart.start_date)) && (b = BlackOut.date_is_blacked_out(cart.due_date))
+      if a == b
+        flash[:error] = a.notice + " Please choose a different start or end date in your Cart."
+      else
+        flash[:error] = a.notice + " " + b.notice + " Please choose a different start or end date in your Cart."
+      end
+    elsif (a = BlackOut.date_is_blacked_out(cart.start_date))
+      flash[:error] = a.notice + " Please choose a different start or end date in your Cart."
+    elsif (a = BlackOut.date_is_blacked_out(cart.due_date))
+      flash[:error] = a.notice + " Please choose a different start or end date in your Cart."
+    end
+  end
+
 end
