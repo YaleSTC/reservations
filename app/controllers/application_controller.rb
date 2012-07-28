dae4e96bb435078265232e5a9edbe8718c2babfa
Machiste Quintana <machiste.quintana@yale.edu>
2012-07-28 15:41:32 -0400

    #216 controllers now only load the helpers that they share a name with. if we want to use other helpers, we just have to specify them explicitly in the controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+  helper :layout
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   before_filter RubyCAS::Filter
