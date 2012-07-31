61b2161cba8f346496ebca368ed341e6c36ef79a
Nick Maas <ncmaas@gmail.com>
2012-07-31 12:45:32 -0400

    fixed Requirements not saving notes

--- app/models/requirement.rb
+++ app/models/requirement.rb
@@ -4,7 +4,7 @@ class Requirement < ActiveRecord::Base
                           :class_name => "User",
                           :association_foreign_key => "user_id",
                           :join_table => "users_requirements" # This join table associates users with the requirements that they have fulfilled. To give a user permission to reserve an item with a requirement, go to their Edit page.
-   attr_accessible :user_id, :equipment_model_id, :contact_info, :contact_name, :requirement_ids, :user_ids, :equipment_model_ids
+   attr_accessible :user_id, :equipment_model_id, :contact_info, :contact_name, :requirement_ids, :user_ids, :equipment_model_ids, :notes
    #serialize :requirement_steps
 
   validates :equipment_model_id, 
