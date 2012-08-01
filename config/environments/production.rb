e911e9cfcc3df54b925cde81ab5ad290ddc67c9b
Adam Bray <adam.bray@yale.edu>
2012-08-01 12:23:39 -0400

    testing fix for asset compilation bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,7 +52,7 @@ Reservations::Application.configure do
 
   # Choose the compressors to use
   config.assets.js_compressor  = :uglifier
-  config.assets.css_compressor = :yui
+  #config.assets.css_compressor = :yui
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
