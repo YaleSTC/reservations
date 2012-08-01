903f46c183d744056f3233e78001928d5aad9f02
Machiste Quintana <machiste.quintana@yale.edu>
2012-08-01 11:47:42 -0400

    removed unnecessary comment at the top of our application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,3 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
 //= require jquery
 //= require jquery_ujs
 //= require jquery.ui.datepicker
