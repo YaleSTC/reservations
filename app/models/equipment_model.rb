4bc3aee985abccfd2d9576ecc615b14bfe5ac79e
Nick Maas <ncmaas@gmail.com>
2012-08-09 09:35:57 -0400

    refactoring complete

--- app/models/equipment_model.rb
+++ app/models/equipment_model.rb
@@ -186,15 +186,27 @@ class EquipmentModel < ActiveRecord::Base
     return false
   end
 
+  def reserved_count(date) #Returns the number of reserved objects for a particular model, as long as they have not been checked in or out
+    Reservation.where("checked_in IS NULL and checked_out IS NULL and equipment_model_id = ? and start_date <= ? and due_date >= ?", self.id, date.to_time.utc, date.to_time.utc).size
+  end
+
+  def overdue_count(date) #Returns the number of overdue objects for a given model, as long as they have been checked out.
+    Reservation.where("checked_in IS NULL and checked_out IS NOT NULL and equipment_model_id = ? and due_date < ?", self.id, Date.today.to_time.utc).size
+  end
+
+  def checked_out(date) #Returns the number of objects for a particular model that are checked out, and not overdue.
+    Reservation.where("checked_in IS NULL and checked_out IS NOT NULL and equipment_model_id = ? and due_date > ?", self.id, Date.today.to_time.utc).size
+  end
+
   def available_count(date)
     # get the total number of objects of this kind
-    # then subtract the total quantity currently checked out, reserved, or overdue
-    # TODO: the system does not account for early checkouts; but early checkouts are no longer possible, so non-issue?
+    # then subtract the total quantity currently checked out, reserved, and overdue
+    reserved_count = self.reserved_count(date)
+    checked_out = self.checked_out(date)
+    overdue_count = self.overdue_count(date)
+    total_count = equipment_objects.count
 
-    reserved_count = Reservation.where("checked_in IS NULL and equipment_model_id = ? and start_date <= ? and due_date >= ?", self.id, date.to_time.utc, date.to_time.utc).size
-    overdue_count = Reservation.where("checked_in IS NULL and checked_out IS NOT NULL and equipment_model_id = ? and due_date < ?", self.id, Date.today.to_time.utc).size
-
-    self.equipment_objects.count - reserved_count - overdue_count
+    total_count - reserved_count - overdue_count - checked_out
   end
 
   def available_object_select_options
