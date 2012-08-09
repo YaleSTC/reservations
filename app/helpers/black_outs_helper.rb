74da0f5742e5863300b547c2821df22ffb458dd0
Nick Maas <ncmaas@gmail.com>
2012-08-09 12:43:13 -0400

    Corrected so that soft notices don't request you change the start or end date.

--- app/helpers/black_outs_helper.rb
+++ app/helpers/black_outs_helper.rb
@@ -2,15 +2,14 @@ module BlackOutsHelper
   def blackout_errors
     if (a = BlackOut.date_is_blacked_out(cart.start_date)) && (b = BlackOut.date_is_blacked_out(cart.due_date))
       if a == b
-        flash[:error] = a.notice + " Please choose a different start or end date in your Cart."
+        flash[:error] = a.notice + ((a.black_out_type == "hard") ? " Please choose different start and end dates in your Cart." : "")
       else
-        flash[:error] = a.notice + " " + b.notice + " Please choose a different start or end date in your Cart."
+        flash[:error] = a.notice + " " + b.notice + ((a.black_out_type == "hard" || (b.black_out_type == "hard")) ? " Please choose a different start or end date in your Cart." : "")
       end
     elsif (a = BlackOut.date_is_blacked_out(cart.start_date))
-      flash[:error] = a.notice + " Please choose a different start or end date in your Cart."
+      flash[:error] = a.notice + ((a.black_out_type == "hard") ? " Please choose a different start date in your Cart." : "")
     elsif (a = BlackOut.date_is_blacked_out(cart.due_date))
-      flash[:error] = a.notice + " Please choose a different start or end date in your Cart."
+      flash[:error] = a.notice + ((a.black_out_type == "hard") ? " Please choose a different end date in your Cart." : "")
     end
   end
-
 end
