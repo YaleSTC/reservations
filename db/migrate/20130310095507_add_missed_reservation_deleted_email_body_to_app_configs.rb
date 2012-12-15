a6a2eeac2376aa4388e652bd0c27ea6ebdca2239
Adam Bray <adam.bray@yale.edu>
2012-12-14 21:01:09 -0500

    updated default deleted res email text for existing app users

--- db/migrate/20130310095507_add_missed_reservation_deleted_email_body_to_app_configs.rb
+++ db/migrate/20130310095507_add_missed_reservation_deleted_email_body_to_app_configs.rb
@@ -5,7 +5,18 @@ class AddMissedReservationDeletedEmailBodyToAppConfigs < ActiveRecord::Migration
     app_config = AppConfig.first
     
     if app_config
-      app_config.deleted_missed_reservation_email_body = "Dear @user@,\nBecause you have missed a scheduled equipment checkout, your reservation has been cancelled. If you believe this is in error, please contact an administrator.\n\nThank you,\n@department_name@"
+      app_config.deleted_missed_reservation_email_body = "Dear @user@,
+
+Equipment reserved for reservation \#@reservation_id@ was due to be picked up on @start_date@  but was not picked up. This reservation has been cancelled. 
+
+The following items were not picked up:
+
+@equipment_list@
+
+If you believe this is in error, please contact an administrator.
+
+Thank you,
+@department_name@"
       app_config.save
     end
 
