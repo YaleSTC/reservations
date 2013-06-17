0f28b671b10260753aa958431361f28bc0fb7e0f
Austin Czarnecki <austin.czarnecki@gmail.com>
2013-06-17 21:22:04 +0000

    Created set_current_requirement filter and removed xml rendering lines

--- app/controllers/requirements_controller.rb
+++ app/controllers/requirements_controller.rb
@@ -1,6 +1,16 @@
 class RequirementsController < ApplicationController
 
   before_filter :require_admin
+  before_filter :set_current_requirement, :only => [:show, :edit, :update, :destroy]
+
+  # ------------- before filter methods ------------- #
+
+  def set_current_requirement
+    @requirement = Requirement.find(params[:id])
+  end
+
+  # ------------- end before filter methods ------------- #
+
 
   # GET /requirements
   # GET /requirements.xml
@@ -9,19 +19,16 @@ class RequirementsController < ApplicationController
 
     respond_to do |format|
       format.html # index.html.erb
-      format.xml  { render :xml => @requirements }
     end
   end
 
   # GET /requirements/1
   # GET /requirements/1.xml
   def show
-    @requirement = Requirement.find(params[:id])
-#    @reqSteps = RequirementStep.order("position")
+    # @reqSteps = RequirementStep.order("position")
 
     respond_to do |format|
       format.html # show.html.erb
-      format.xml  { render :xml => @requirement }
     end
   end
 
@@ -32,13 +39,11 @@ class RequirementsController < ApplicationController
 
     respond_to do |format|
       format.html # new.html.erb
-      format.xml  { render :xml => @requirement }
     end
   end
 
   # GET /requirements/1/edit
   def edit
-    @requirement = Requirement.find(params[:id])
   end
 
   # POST /requirements
@@ -48,10 +53,8 @@ class RequirementsController < ApplicationController
      respond_to do |format|
       if @requirement.save
         format.html { redirect_to(@requirement, :notice => 'Requirement was successfully created.') }
-        format.xml  { render :xml => @requirement, :status => :created, :location => @requirement }
       else
         format.html { render :action => "new" }
-        format.xml  { render :xml => @requirement.errors, :status => :unprocessable_entity }
       end
     end
   end
@@ -59,15 +62,11 @@ class RequirementsController < ApplicationController
   # PUT /requirements/1
   # PUT /requirements/1.xml
   def update
-    @requirement = Requirement.find(params[:id])
-
     respond_to do |format|
       if @requirement.update_attributes(params[:requirement])
         format.html { redirect_to(@requirement, :notice => 'Requirement was successfully updated.') }
-        format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
-        format.xml  { render :xml => @requirement.errors, :status => :unprocessable_entity }
       end
     end
   end
@@ -75,12 +74,10 @@ class RequirementsController < ApplicationController
   # DELETE /requirements/1
   # DELETE /requirements/1.xml
   def destroy
-    @requirement = Requirement.find(params[:id])
     @requirement.destroy(:force)
 
     respond_to do |format|
       format.html { redirect_to(requirements_url) }
-      format.xml  { head :ok }
     end
   end
 end
