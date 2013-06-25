984e43382a2b953e8e30e4fa7e86c1d42dad2816
Simon Podhajsky <simon.podhajsky@gmail.com>
2013-06-25 14:59:34 -0400

    AppConfig tests

--- spec/models/app_config_spec.rb
+++ spec/models/app_config_spec.rb
@@ -4,9 +4,19 @@ describe AppConfig do
   before(:each) do
     @ac = FactoryGirl.build(:app_config)
   end
-  it "has a working factory"
-  it "has a site title"
-  it "has a short enough site title"
+  it "has a working factory" do
+    @ac.save.should be_true
+  end
+  it "does not accept empty site title" do
+    @ac.site_title = nil
+    @ac.should_not be_valid
+    @ac.site_title = " "
+    @ac.should_not be_valid
+  end
+  it "does not accept too long a site title" do
+    @ac.site_title = "AaaaaBbbbbCccccDddddEeeee"
+    @ac.should_not be_valid
+  end
   it "shouldn't have an invalid e-mail" do
     emails = ["ana@com", "anda@pres,com", nil, " "]
     emails.each do |invalid|
@@ -18,6 +28,20 @@ describe AppConfig do
     @ac.admin_email = "ana@yale.edu"
     @ac.should be_valid
   end
-  it "has an attachment that could serve as favicon"
-  it "has an attachment that is of the favicon format"
+  # it "has an attachment that could serve as favicon" do
+  #   @ac.favicon_file_name = "icon.ico"
+  #   @ac.should be_valid
+  # end
+  # it "does not accept a missing favicon" do
+  #   @ac.favicon_file_name = ""
+  #   @ac.should_not be_valid
+  # end
+  it "has an attachment that is of the favicon format" do
+    types = ["image/gif", "image/jpeg", "image/png"]
+    types.each do |invalid|
+      @ac.favicon_content_type = invalid
+      @ac.should_not be_valid
+    end
+    @ac.favicon_content_type = "image/vnd.microsoft.icon"
+  end
 end
