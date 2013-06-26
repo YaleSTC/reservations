b7e5a8b1031bf08be696c696e5bd4077ffb0531c
Austin Czarnecki <austin.czarnecki@gmail.com>
2013-06-26 17:02:59 +0000

    Finished writing tests for EquipmentObject model

--- spec/models/equipment_object_spec.rb
+++ spec/models/equipment_object_spec.rb
@@ -10,20 +10,9 @@ describe EquipmentObject do
       @object.save.should be_true
     end
 
-    it "must have a name" do
-      @object.name = ""
-      @object.save.should be_false
-      @object.name = "Number FGH4567"
-      @object.save.should be_true
-    end
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:equipment_model) }
 
-    it "must have an equipment model with which it is associated" do
-      @model = @object.equipment_model
-      @object.equipment_model = nil
-      @object.save.should be_false
-      @object.equipment_model = @model
-      @object.save.should be_true
-    end
     # this test passes even without the nilify_blanks call in the model, maybe delete the call?
     it "saves an empty string value as nil for deleted_at field" do
       @object.deleted_at = "   "
@@ -47,22 +36,48 @@ describe EquipmentObject do
     end
   end
 
-  # wait to do these until this is merged with the reservations tests/factories
-
   describe ".status" do
-    it "returns 'deactivated' if the object has a value for deleted_at"
-    it "returns 'available' if the object is active and not currently checked out"
-    # "checked out by #{r.reserver.name} through #{r.due_date.strftime("%b %d")}"
-    it "returns the reservation that it is currently associated with if it is active and checked out"
+    it "returns 'Deactivated' if the object has a value for deleted_at" do
+      @object = FactoryGirl.create(:equipment_object, deleted_at: Date.today)
+      @object.status.should == 'Deactivated'
+    end
+    it "returns 'available' if the object is active and not currently checked out" do
+      @object = FactoryGirl.create(:equipment_object)
+      @object.status.should == 'available'
+      @reservation = FactoryGirl.create(:reservation)
+      @reserved_object = @reservation.equipment_object
+      @reserved_object.status.should == 'available'
+    end
+    it "returns a description of the reservation that it is currently associated with if it is active and checked out" do
+      @reservation = FactoryGirl.create(:checked_out_reservation)
+      @checked_out_object = @reservation.equipment_object
+      @checked_out_object.status.should == "checked out by #{@reservation.reserver.name} through #{@reservation.due_date.strftime("%b %d")}"
+    end
   end
 
   describe ".current_reservation" do
-    it "returns nil if the equipment object does not have an associted reservation"
-    it "returns the reservation object currently holding this equipment_object if there is one that does"
+    it "returns nil if the equipment object does not have an associted reservation" do
+      @object = FactoryGirl.create(:equipment_object)
+      @object.current_reservation.should be_nil
+    end
+    it "returns the reservation object currently holding this equipment_object if there is one that does" do
+      @reservation = FactoryGirl.create(:checked_out_reservation)
+      @reserved_object = @reservation.equipment_object
+      @reserved_object.current_reservation.should == @reservation
+    end
   end
 
   describe ".available?" do
-    it "returns true if the equipment object is not checked out"
-    it "returns false if the equipment object is currently checked out"
+    before(:each) do
+      @reservation = FactoryGirl.create(:checked_out_reservation)
+      @checked_out_object = @reservation.equipment_object
+      @object = FactoryGirl.create(:equipment_object)
+    end
+    it "returns true if the equipment object is not checked out" do
+      @object.available?.should be_true
+    end
+    it "returns false if the equipment object is currently checked out" do
+      @checked_out_object.available?.should be_false
+    end
   end
 end
