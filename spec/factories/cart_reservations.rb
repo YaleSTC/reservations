2997252bedbd0cae524122eb57b56240d413b22c
ebmaher <erin.maher@yale.edu>
2013-06-28 15:21:33 -0400

    bugfix to cart_res factory

--- spec/factories/cart_reservations.rb
+++ spec/factories/cart_reservations.rb
@@ -6,5 +6,13 @@ FactoryGirl.define do
     due_date { Date.tomorrow }
     reserver
     equipment_model
+
+    factory :valid_cart_reservation do
+      after(:build) do |cart_res|
+        if cart_res.equipment_model.equipment_objects.empty?
+          FactoryGirl.create(:equipment_object, equipment_model: res.equipment_model)
+        end
+      end
+    end
   end
 end
