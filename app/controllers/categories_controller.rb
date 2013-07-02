5bb032b1a028fef6a0ec3ac3dbf98a24c0803575
Austin Czarnecki <austin.czarnecki@gmail.com>
2013-07-02 17:36:03 +0000

    Deleted useless code from categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -29,7 +29,6 @@ class CategoriesController < ApplicationController
 
   def create
     @category = Category.new(params[:category])
-    @category.sort_order = params[:category][:sort_order].to_i
     if @category.save
       flash[:notice] = "Successfully created category."
       redirect_to @category
@@ -43,7 +42,6 @@ class CategoriesController < ApplicationController
   end
 
   def update
-    @category.sort_order = params[:category][:sort_order].to_i
     if @category.update_attributes(params[:category])
       flash[:notice] = "Successfully updated category."
       redirect_to @category
