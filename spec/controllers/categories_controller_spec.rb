b71871986249d04d239f7a8be82d431db1939945
Austin Czarnecki <austin.czarnecki@gmail.com>
2013-07-02 00:53:14 +0000

    Added bare-bones spec for categories controller

---
+++ spec/controllers/categories_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe CategoriesController do
+  describe 'GET index' do
+  end
+  describe 'GET show' do
+  end
+  describe 'GET new' do
+  end
+  describe 'POST create' do
+  end
+  describe 'GET edit' do
+  end
+  describe 'PUT update' do
+  end
+  describe 'DELETE destroy' do
+  end
+end
