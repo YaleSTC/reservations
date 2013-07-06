9d1c8cb6a79a29591f6cae93ed11099d800176ee
Austin Czarnecki <austin.czarnecki@gmail.com>
2013-07-06 22:14:47 +0000

    Changed all 'match' requests in routes to specific routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,8 +14,8 @@ Reservations::Application.routes.draw do
     resources :equipment_objects
   end
 
-  match '/import_users/import' => 'import_users#import_page', :via => :get, :as => :csv_import_page
-  match '/import_users/imported' => 'import_users#import', :via => :post, :as => :csv_imported
+  get '/import_users/import' => 'import_users#import_page', :via => :get, :as => :csv_import_page
+  post '/import_users/imported' => 'import_users#import', :via => :post, :as => :csv_imported
 
   resources :users do
     collection do
@@ -23,8 +23,8 @@ Reservations::Application.routes.draw do
     end
   end
 
-  match '/catalog/search' => 'catalog#search', :as => :catalog_search
-  match '/markdown_help' => 'application#markdown_help', :as => :markdown_help
+  match '/catalog/search' => 'catalog#search', :as => :catalog_search # what kind of http request is this?
+  get '/markdown_help' => 'application#markdown_help', :as => :markdown_help
 
   resources :reservations do
     member do
@@ -35,8 +35,8 @@ Reservations::Application.routes.draw do
     get :autocomplete_user_last_name, :on => :collection
   end
 
-  match '/blackouts/flash_message' => 'blackouts#flash_message', :as => :flash_message
-  match '/blackouts/new_recurring' => 'blackouts#new_recurring', :as => :new_recurring_blackout
+  get '/blackouts/flash_message' => 'blackouts#flash_message', :as => :flash_message
+  get '/blackouts/new_recurring' => 'blackouts#new_recurring', :as => :new_recurring_blackout
 
   resources :blackouts do
     collection do
@@ -49,47 +49,47 @@ Reservations::Application.routes.draw do
   end
 
   # reservations views
-  match '/reservations/manage/:user_id' => 'reservations#manage', :as => :manage_reservations_for_user
-  match '/reservations/current/:user_id' => 'reservations#current', :as => :current_reservations_for_user
+  get '/reservations/manage/:user_id' => 'reservations#manage', :as => :manage_reservations_for_user
+  get '/reservations/current/:user_id' => 'reservations#current', :as => :current_reservations_for_user
 
 
   # reservation checkout / check-in actions
-  match '/reservations/checkout/:user_id' => 'reservations#checkout', :via => :put, :as => :checkout
-  match '/reservations/check-in/:user_id' => 'reservations#checkin', :via => :put, :as => :checkin
+  put '/reservations/checkout/:user_id' => 'reservations#checkout', :via => :put, :as => :checkout
+  put '/reservations/check-in/:user_id' => 'reservations#checkin', :via => :put, :as => :checkin
 
-  match '/catalog/update_view' => 'catalog#update_user_per_cat_page', :as => :update_user_per_cat_page
-  match '/catalog' => 'catalog#index', :as => :catalog
-  match '/add_to_cart/:id' => 'catalog#add_to_cart', :via => :put, :as => :add_to_cart
-  match '/remove_from_cart/:id' => 'catalog#remove_from_cart', :via => :put, :as => :remove_from_cart
-  match '/cart/empty' => 'application#empty_cart', :via => :delete, :as => :empty_cart
-  match '/cart/update' => 'application#update_cart', :as => :update_cart
+  put '/catalog/update_view' => 'catalog#update_user_per_cat_page', :as => :update_user_per_cat_page
+  get '/catalog' => 'catalog#index', :as => :catalog
+  put '/add_to_cart/:id' => 'catalog#add_to_cart', :via => :put, :as => :add_to_cart
+  put '/remove_from_cart/:id' => 'catalog#remove_from_cart', :via => :put, :as => :remove_from_cart
+  delete '/cart/empty' => 'application#empty_cart', :via => :delete, :as => :empty_cart
+  put '/cart/update' => 'application#update_cart', :as => :update_cart
 
-  match '/reports/index' => 'reports#index', :as => :reports
-  match '/reports/:id/for_model' => 'reports#for_model', :as => :for_model_report
+  get '/reports/index' => 'reports#index', :as => :reports
+  get '/reports/:id/for_model' => 'reports#for_model', :as => :for_model_report
   match '/reports/for_model_set' => 'reports#for_model_set', :as => :for_model_set_reports
   match '/reports/update' => 'reports#update_dates', :as => :update_dates
-  match '/reports/generate' => 'reports#generate', :as => :generate_report
+  put '/reports/generate' => 'reports#generate', :as => :generate_report
 
-  match '/:controller/:id/deactivate' => ':controller#deactivate', :via => :put, :as => 'deactivate'
-  match '/:controller/:id/activate' => ':controller#activate', :via => :put, :as => 'activate'
+  put '/:controller/:id/deactivate' => ':controller#deactivate', :via => :put, :as => 'deactivate'
+  put '/:controller/:id/activate' => ':controller#activate', :via => :put, :as => 'activate'
 
-  match '/logout' => 'application#logout', :as => :logout
+  match '/logout' => 'application#logout', :as => :logout # what kind of http request is this?
 
-  match '/terms_of_service' => 'application#terms_of_service', :as => :tos
+  match '/terms_of_service' => 'application#terms_of_service', :as => :tos # see what this method actually does
 
   # yes, both of these are needed to override rails defaults of /controller/:id/edit
-  match '/app_configs/' => 'app_configs#edit', :as => :edit_app_configs
+  get '/app_configs/' => 'app_configs#edit', :as => :edit_app_configs
   resources :app_configs, :only => [:update]
 
-  match '/new_admin_user' => 'application_setup#new_admin_user', :as => :new_admin_user
-  match '/create_admin_user' => 'application_setup#create_admin_user', :as => :create_admin_user
+  get '/new_admin_user' => 'application_setup#new_admin_user', :as => :new_admin_user
+  post '/create_admin_user' => 'application_setup#create_admin_user', :as => :create_admin_user
   resources :application_setup, :only => [:new_admin_user, :create_admin_user]
 
-  match '/new_app_configs' => 'application_setup#new_app_configs', :as => :new_app_configs
-  match '/create_app_configs' => 'application_setup#create_app_configs', :as => :create_app_configs
+  get '/new_app_configs' => 'application_setup#new_app_configs', :as => :new_app_configs
+  post '/create_app_configs' => 'application_setup#create_app_configs', :as => :create_app_configs
 
-  match 'contact' => 'contact#new', :as => 'contact_us', :via => :get
-  match 'contact' => 'contact#create', :as => 'contact_us', :via => :post
+  get 'contact' => 'contact#new', :as => 'contact_us', :via => :get
+  post 'contact' => 'contact#create', :as => 'contact_us', :via => :post
 
   match ':controller(/:action(/:id(.:format)))'
 
