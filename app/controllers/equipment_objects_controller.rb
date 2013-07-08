e13396bd250b9bd7165f2a5da299e55a8337aac9
ebmaher <erin.maher@yale.edu>
2013-07-07 21:26:02 -0400

    added comments to indicate possible problems with equipment objects controller index code

--- app/controllers/equipment_objects_controller.rb
+++ app/controllers/equipment_objects_controller.rb
@@ -18,11 +18,13 @@ class EquipmentObjectsController < ApplicationController
 
   # ---------- end before filter methods ---------- #
 
-
+  # I think this code is wrong
   def index
     if params[:show_deleted]
+      #should be:        = ( @equipment_model  ? @equipment_model.equipment_objects : EquipmentObject.all )
       @equipment_objects = ( @equipment_object ? @equipment_object.equipment_models : EquipmentObject.all )
     else
+      #should be:        = ( @equipment_model  ? @equipment_model.equipment_objects.active : EquipmentObject.active )
       @equipment_objects = ( @equipment_object ? @equipment_object.equipment_models.active : EquipmentObject.active )
     end
   end
