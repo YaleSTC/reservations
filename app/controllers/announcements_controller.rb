3d043c06a63f3d8e2467cb04a6c8b5a28218e948
kboateng3 <kboateng3@gmail.com>
2013-07-30 14:21:56 -0400

    fixed announcements view, but announcements still aren't visible

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -7,4 +7,40 @@ class AnnouncementsController < ApplicationController
       format.js
     end
   end
+
+  def index
+  	@announcements = Announcement.all
+  end
+
+  def new
+  	if current_user and current_user.is_admin?(:as => 'admin')
+  		@announcement = Announcement.new
+  	end
+  end
+
+  def create
+  	@announcement = Announcement.new(params[:announcement])
+  	if @announcement.save
+  		respond_to do |format|
+  			flash[:notice] = "Successfully created announcement."
+  			format.js {render :aciton => 'create_success'}
+  		end
+  	end
+  end
+
+  def update
+      if @announcement.update_attributes(params[:announcement])
+      	respond_to do |format|   
+        format.html { redirect_to @announcement, notice: 'Announcement was successfully updated.' }
+  		format.js {render :aciton => 'create_success'}
+      	end
+      end
+  end
+
+  def delete
+  	@announcement.destroy
+  	# respond_to do |format|
+  	# 	format.html {}
+  end
+
 end
