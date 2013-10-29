a66f9db6ab5e332b69663df11848172e9557a01e
Adam Bray <adam.bray@yale.edu>
2013-10-29 15:44:47 -0400

    fixed logic errog in migration order

--- db/migrate/20130708215903_add_viewed_to_app_configs.rb
+++ db/migrate/20130708215903_add_viewed_to_app_configs.rb
@@ -1,8 +1,6 @@
 class AddViewedToAppConfigs < ActiveRecord::Migration
   def up
-    add_column :app_configs, :viewed, :boolean, :default => true
   end
   def down
-    remove_column :app_configs, :viewed
   end
 end
