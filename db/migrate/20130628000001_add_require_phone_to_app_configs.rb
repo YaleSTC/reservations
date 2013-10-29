87222edfd7824ce19001d5993c83734466d789f3
Adam Bray <adam.bray@yale.edu>
2013-10-29 15:21:35 -0400

    re-ordered migration to fix a bug

--- db/migrate/20130628000001_add_require_phone_to_app_configs.rb
+++ db/migrate/20130628000001_add_require_phone_to_app_configs.rb
@@ -1,3 +1,7 @@
+# Note, this migration was moved forward in time (timestamp) to fix bug with
+# next migration (combine_type_columns_in_users_table) which referenced this
+# attr through the user model.
+
 class AddRequirePhoneToAppConfigs < ActiveRecord::Migration
   def up
   	add_column :app_configs, :require_phone, :boolean, :default => true
