14acc3c497c5738a8355d1b3e5d751fb640cda6a
Oren Kanner <okanner@gmail.com>
2014-03-26 20:54:30 -0400

    started filling in controller, put together import view

--- app/controllers/import_equipment_controller.rb
+++ app/controllers/import_equipment_controller.rb
@@ -3,10 +3,19 @@ class ImportEquipmentController < ApplicationController
 
   before_filter :require_admin
 
+  # modeled after the ImportUsersController
   def import
+  	# initialize, we take up to three CSV files, now we have to check each
+  	binding.pry
+  	cat_file = params[:cat_upload]
+  	model_file = params[:model_upload]
+  	object_file = params[:object_upload]
+
+  	render 'imported'
   end
 
   def import_page
+  	render 'import'
   end
 
   private
