ac03cf735cc1610e0e70f8cad8a53e0e20e93d0a
Oren Kanner <okanner@gmail.com>
2014-03-26 21:03:44 -0400

    added strategy comments to controller

--- app/controllers/import_equipment_controller.rb
+++ app/controllers/import_equipment_controller.rb
@@ -11,6 +11,16 @@ class ImportEquipmentController < ApplicationController
   	model_file = params[:model_upload]
   	object_file = params[:object_upload]
 
+  	# OK, so we need to check each file in turn, if it exists save the overwrite parameter and file path, process the CSV file, validate the processed data, attempt to import the data, and then report the import status
+
+  	# we can definitely avoid duplicating the initial code (check for file, store overwrite, process CSV file); maybe store the three files in an array and just loop over it
+
+  	# once we've processed any categories, we then move on to equipment models, making sure to search for the associated categories by name and store the id of the categories (if found)
+
+  	# similarly, after processing any equipment models, we move on to equipment items and search for the associated equipment models by name and store the id (if found)
+
+  	# finally, we need to put together a relatively clean view to summarize the results of each requested import operation.
+
   	render 'imported'
   end
 
