c84e76dc40ccf0f18e97bd0aaddc7d7ee99c91af
Oren Kanner <okanner@gmail.com>
2014-04-18 15:12:57 -0400

    getting tests to work

---
+++ spec/lib/equipment_import_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+# include the module file
+require "#{Rails.root}/lib/equipment_import.rb"
+
+describe EquipmentImport do
+	pending "write tests"
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -41,4 +41,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  # look for files outside the usual, e.g. lib/*_spec.rb
+  config.pattern = "**/*_spec.rb"
 end
