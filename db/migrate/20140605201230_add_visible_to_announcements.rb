f52a8510b11ecfb2ed0ce650d321e077a0d44c22
Sylvan Zheng <squidgetx@gmail.com>
2014-06-06 10:20:12 -0400

    #447 deleted unnecessary migrations

--- db/migrate/20140605201230_add_visible_to_announcements.rb
+++
@@ -1,5 +0,0 @@
-class AddVisibleToAnnouncements < ActiveRecord::Migration
-  def change
-    add_column :announcements, :visible_to, :string
-  end
-end
--- db/migrate/20140605203105_remove_visible_from_announcements.rb
+++
@@ -1,9 +0,0 @@
-class RemoveVisibleFromAnnouncements < ActiveRecord::Migration
-  def up
-    remove_column :announcements, :visible_to
-  end
-
-  def down
-    add_column :announcements, :visible_to, :string
-  end
-end
