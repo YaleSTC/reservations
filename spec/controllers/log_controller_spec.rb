60838f9b1a7aaee0c904afd28143019e0a602523
Simon Podhajsky <simon.podhajsky@gmail.com>
2014-06-10 18:01:22 -0400

    Outlined basic tests for controller functions and the transform_attributes helper

--- spec/controllers/log_controller_spec.rb
+++ spec/controllers/log_controller_spec.rb
@@ -9,4 +9,27 @@ describe LogController do
     end
   end
 
+  describe "GET 'version/:id'" do
+    it "returns http success if :id exists" do
+      get 'version/1'
+      response.should be_success
+    end
+
+    it "redirects to index if :id doesn't exist" do
+      get 'version/9999'
+      response.should be_redirect
+    end
+  end
+
+  describe "GET 'history/:id'" do
+    it "returns http success if Reservation :id exists" do
+      get 'history/1'
+      response.should be_success
+    end
+
+    it "redirects to index if Reservation :id doesn't exist" do
+      get 'history/9999'
+      response.should be_redirect
+    end
+  end
 end
