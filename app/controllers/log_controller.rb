0a7d55e5116def0d8c743b3e3db4097adbe8caaf
Simon Podhajsky <simon.podhajsky@gmail.com>
2014-06-10 09:49:24 -0400

    Added a paper_trail-version-dependent notice

--- app/controllers/log_controller.rb
+++ app/controllers/log_controller.rb
@@ -1,5 +1,7 @@
 class LogController < ApplicationController
   def index
+    # Past paper_trail 2.7-stable, Version is namespaced as PaperTrail::Version
+    # and so this line will break.
     @all_versions = Version.order("id desc").all
   end
 end
