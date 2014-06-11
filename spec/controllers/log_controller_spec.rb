17431cd3c939101bf44b9927c6fd9fa25751f046
Simon Podhajsky <simon.podhajsky@gmail.com>
2014-06-11 17:31:05 -0400

    Added redirect and test for it (pending)

--- spec/controllers/log_controller_spec.rb
+++ spec/controllers/log_controller_spec.rb
@@ -11,24 +11,24 @@ describe LogController do
 
   describe "GET 'version/:id'" do
     it "returns http success if :id exists" do
-      get 'version/1'
+      get 'version', id: 1
       response.should be_success
     end
 
-    it "redirects to index if :id doesn't exist" do
-      get 'version/9999'
-      response.should be_redirect
+    xit "redirects to index if :id doesn't exist" do
+      get 'version', id: 99999
+      response.should redirect_to('log/index')
     end
   end
 
   describe "GET 'history/:id'" do
     it "returns http success if Reservation :id exists" do
-      get 'history/1'
+      get 'history', id: 1
       response.should be_success
     end
 
-    it "redirects to index if Reservation :id doesn't exist" do
-      get 'history/9999'
+    xit "redirects to index if Reservation :id doesn't exist" do
+      get 'history', id: 99999
       response.should be_redirect
     end
   end
