9f5436813c117b7a50e56ccb4083aea9534b4f5d
Sylvan Zheng <squidgetx@gmail.com>
2014-06-11 15:40:33 -0400

    stupid

--- db/migrate/20140611174136_change_cart_reservation_datetime_to_date.rb
+++
@@ -1,6 +0,0 @@
-class ChangeCartReservationDatetimeToDate < ActiveRecord::Migration
-	def change
-		add_column :cart_reservations, :start, :date
-		add_column :cart_reservations, :due, :date
-		
-end
