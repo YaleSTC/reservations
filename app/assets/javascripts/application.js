94a66daf4795970116c81817f4d5a8d3ad354896
Sylvan Zheng <squidgetx@gmail.com>
2014-06-12 14:37:35 -0400

    typo

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -108,7 +108,7 @@
 
 $(document).ready(function() {
   
-  $('.checkin-click').click( function(e) {
+  $('.checkin-click').click( function() {
 	var box = $(this).find(":checkbox");
 	box.prop("checked", !box.prop("checked"))
 	if ($(this).hasClass("overdue")) {
