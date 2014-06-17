63b1655ec47f0e3fe61893e14f6b2b7c6933c5e1
Sylvan Zheng <squidgetx@gmail.com>
2014-06-17 12:02:39 -0400

    removed delete button res show, added catch for 0 users

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,44 +2,44 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-   
-    case user.view_mode
-		#when 'superuser'
-			#can :manage, :all
-		when 'admin'
-			can :manage, :all
-			#cannot :appoint, :superuser
-			#cannot :manage, Admin
-		when 'checkout'
-			can :manage, Reservation
-      cannot [:destroy, :edit], Reservation do |r|
-         r.checked_out != nil
-      end
-			unless AppConfig.first.checkout_persons_can_edit
-				cannot :update, Reservation
-			end
-			can :read, User
-			can :update, User
-			can :create, User
-			can :read, Category
-			can :read, EquipmentObject
-			can :read, EquipmentModel
-			if AppConfig.first.override_on_create
-				can :override, :reservation_errors
-			end
-			if AppConfig.first.override_at_checkout
-				can :override, :checkout_errors
-			end
-		when 'normal' || 'checkout'
-			can :read, User, :id => user.id
-			can :create, User, :id => user.id
-			can :read, Reservation, :reserver_id => user.id
-			can :create, Reservation, :reserver_id => user.id
-			can :destroy, Reservation, :reserver_id => user.id, :checked_out => nil
-			can :read, Reservation, :reserver_id => user.id
-		when 'banned'
-			cannot :create, Reservation
-	end
-    
+    if user
+      case user.view_mode
+	    	#when 'superuser'
+		  	#can :manage, :all
+	  	when 'admin'
+		  	can :manage, :all
+		  	#cannot :appoint, :superuser
+		  	#cannot :manage, Admin
+		  when 'checkout'
+			  can :manage, Reservation
+        cannot :destroy, Reservation do |r|
+           r.checked_out != nil
+        end
+			  unless AppConfig.first.checkout_persons_can_edit
+		  		cannot :update, Reservation
+		  	end
+			  can :read, User
+			  can :update, User
+			  can :create, User
+			  can :read, Category
+			  can :read, EquipmentObject
+			  can :read, EquipmentModel
+		  	if AppConfig.first.override_on_create
+	  			can :override, :reservation_errors
+  			end
+			  if AppConfig.first.override_at_checkout
+		  		can :override, :checkout_errors
+	  		end
+  		when 'normal' || 'checkout'
+			  can :read, User, :id => user.id
+			  can :create, User, :id => user.id
+			  can :read, Reservation, :reserver_id => user.id
+			  can :create, Reservation, :reserver_id => user.id
+		  	can :destroy, Reservation, :reserver_id => user.id, :checked_out => nil
+	  		can :read, Reservation, :reserver_id => user.id
+  		when 'banned'
+			  cannot :create, Reservation
+	    end
+    end  
   end
 end
