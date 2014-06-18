8b92aa89d0c15e326be4a416c25ada8e19a1270e
Simon Podhajsky <simon.podhajsky@gmail.com>
2014-06-18 12:45:55 -0400

    Added the last couple tests for  helper

--- spec/helpers/log_helper_spec.rb
+++ spec/helpers/log_helper_spec.rb
@@ -52,12 +52,36 @@ describe "transform_attributes" do
       transform_attributes( ['reservation_id', 0] )[1].should == "0 (deleted)"
     end
 
-    pending "converts existing equipment model ID into proper link"
-    pending "converts existing equipment item ID into proper link"
-    pending "converts start date and due date to readable form (Mon DD, YYYY)"
-    pending "converts creation, update, check-in, and check-out time to readable form"
+    it "converts existing equipment model ID into proper link" do
+      eqm = FactoryGirl.create(:equipment_model)
+      transform_attributes( ['equipment_model_id', eqm.id] )[1].should == link_to("#{eqm.name}", EquipmentModel.find(eqm.id))
+    end
 
-    pending "converts other field values to N/A if nil"
-    pending "leaves other field values intact"
+    it "doesn't convert defunct equipment model into a link" do
+      transform_attributes( ['equipment_model_id', 0] )[1].should == "Model ID 0 (deleted)"
+    end
+
+    it "converts existing equipment object ID into proper link" do
+      eqo = FactoryGirl.create(:equipment_object)
+      transform_attributes( ['equipment_object_id', eqo.id] )[1].should == link_to("#{eqo.name}", EquipmentObject.find(eqo.id))
+    end
+
+    it "doesn't convert defunct equipment object into a link" do
+      transform_attributes( ['equipment_object_id', 0] )[1].should == "N/A"
+    end
+
+    it "converts start date and due date to readable form (Mon DD, YYYY)" do
+      date = DateTime.now
+      %w(start_date due_date).each do |k|
+        transform_attributes( [k, date] )[1].should == date.strftime("%B %d, %Y")
+      end
+    end
+
+    it "converts creation, update, check-in, and check-out time to readable form" do
+      date = DateTime.now
+      %w(created_at updated_at checked_in checked_out).each do |k|
+        transform_attributes( [k, date] )[1].should == date.strftime("%B %d, %Y, %H:%M:%S, %z")
+      end
+    end
   end
 end
