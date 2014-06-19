8c1486364595123531e4293ff5f6494b18a476de
Simon Podhajsky <simon.podhajsky@gmail.com>
2014-06-19 14:26:52 -0400

    Conciser href concatenation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -348,5 +348,5 @@ $(document).on('railsAutocomplete.select', '#fake_reserver_id', function(event, 
 
 function getDeactivationReason(e) {
   var p = prompt("Write down the reason for deactivation of this equipment object.")
-  e.href = e.href + "?deactivation_reason=" + encodeURIComponent(p)
+  e.href += "?deactivation_reason=" + encodeURIComponent(p)
 }
