399e3953bf63300d545bcc9d60cc52f1e00ffd16
Simon Podhajsky <simon.podhajsky@gmail.com>
2014-06-24 19:05:34 -0400

    cart_with_items now actually behaves as if it had some items

--- spec/factories/carts.rb
+++ spec/factories/carts.rb
@@ -6,12 +6,7 @@ FactoryGirl.define do
     items []
 
     factory :cart_with_items do
-      ignore do
-        items_count 2
-      end
-      after(:create) do |cart, evaluator|
-        FactoryGirl.create_list(:cart_reservation, evaluator.items_count, cart: cart)
-      end
+      items { [ FactoryGirl.create(:cart_reservation).id ]}
     end
   end
 end
