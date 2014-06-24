0e2df4acf99229ff1e198798c0d8460be6abf191
Sylvan Zheng <squidgetx@gmail.com>
2014-06-24 14:04:27 -0400

    extended string to_time and to_datetime function

--- config/initializers/string.rb
+++ config/initializers/string.rb
@@ -3,7 +3,38 @@ class String
     begin
       Date.strptime(self, '%m/%d/%Y')
     rescue ArgumentError
-      Date.strptime(self, '%Y-%m-%d')
+      Date.parse(self, false) unless blank?
     end
   end
+
+  def to_datetime
+    begin
+      DateTime.strptime(self,'%m/%d/%Y')
+    rescue ArgumentError
+      DateTime.parse(self, false) unless blank?
+    end
+  end
+
+  def to_time(form = :local)
+    begin
+      Time.strptime(self,'%m/%d/%Y')
+    rescue ArgumentError
+      parts = Date._parse(self, false)
+      return if parts.empty?
+
+      now = Time.now
+      time = Time.new(
+        parts.fetch(:year, now.year),
+        parts.fetch(:mon, now.month),
+        parts.fetch(:mday, now.day),
+        parts.fetch(:hour, 0),
+        parts.fetch(:min, 0),
+        parts.fetch(:sec, 0) + parts.fetch(:sec_fraction, 0),
+        parts.fetch(:offset, form == :utc ? 0 : nil)
+      )
+
+      form == :utc ? time.utc : time.getlocal
+    end
+  end
+
 end
