ac8ea5661584d1c591471cb88d27107aaf5a7935
Simon Podhajsky <simon.podhajsky@gmail.com>
2014-06-25 16:48:36 -0400

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140617164155) do
+ActiveRecord::Schema.define(:version => 20140618173229) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -216,7 +216,7 @@ ActiveRecord::Schema.define(:version => 20140617164155) do
     t.string   "login"
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "nickname"
+    t.string   "nickname",                  :default => "",       :null => false
     t.string   "phone"
     t.string   "email"
     t.string   "affiliation"
