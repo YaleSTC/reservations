731d237d4daaa709f9cb3e6ce907c74547ee4ff7
Sylvan Zheng <squidgetx@gmail.com>
2014-06-30 15:26:17 -0400

    scaffolding tests

---
+++ spec/mailers/admin_mailer_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+describe AdminMailer do
+  describe 'notes_reservation_notification'
+  describe 'overdue_checked_in_fine_admin'
+end
---
+++ spec/mailers/user_mailer_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe UserMailer do
+  describe 'checkin_receipt'
+  describe 'checkout_receipt'
+  describe 'missed_reservation_deleted_notification'
+  describe 'overdue_checkin_notification'
+  describe 'overdue_checked_in_fine'
+  describe 'reservation_confirmation'
+  describe 'upcoming_checkin_notification'
+
+end
