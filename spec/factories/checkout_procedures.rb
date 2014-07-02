89098624a49b4890ca1b57434a76947eb5cf6be4
Simon Podhajsky <simon.podhajsky@gmail.com>
2014-07-02 14:59:25 -0400

    Added checking/checkout procedure factories

---
+++ spec/factories/checkout_procedures.rb
@@ -0,0 +1,12 @@
+FactoryGirl.define do
+  factory :checkout_procedure do
+    equipment_model_id { FactoryGirl.create(:equipment_model).id }
+    step "This is a step of the check-out procedure."
+    created_at { Date.yesterday }
+    updated_at { Date.today }
+
+    trait :deleted do
+      deleted_at { Date.today }
+    end
+  end
+end
