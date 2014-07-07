78d50aaa6fe01ab5c5ba8de79ae8321d665efd36
Sylvan Zheng <squidgetx@gmail.com>
2014-07-07 17:18:05 -0400

    Remove cookie_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Reservations::Application.config.session_store :cookie_store, key: '_app_session'
+Reservations::Application.config.session_store :active_record_store
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
