2c7dcd7f5d511a22413fe406d129a363b4dd710c
dgoerger <david.goerger@yale.edu>
2014-07-10 18:12:59 -0400

    fixed migrations and schema for 3.2.x

--- db/migrate/20140306014551_add_default_value_to_users_nickname.rb
+++ db/migrate/20140306014551_add_default_value_to_users_nickname.rb
@@ -1,5 +1,15 @@
 class AddDefaultValueToUsersNickname < ActiveRecord::Migration
-  def self.up
+  def up
+    User.all.each do |user|
+      if user.nickname.nil?
+        user.nickname = ''
+        user.save!
+      end
+    end
   	change_column :users, :nickname, :string, :default => '', :null => false
+  end
+  
+  def down
+    change_column :users, :nickname, :string
   end
 end
