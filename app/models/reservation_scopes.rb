c5c86e8f9bbc8dbdb55e30770dd1f667fa6b8b42
squidgetx <squidgetx@gmail.com>
2014-07-14 15:45:28 -0400

    scope simplification

--- app/models/reservation_scopes.rb
+++ app/models/reservation_scopes.rb
@@ -1,21 +1,24 @@
 module ReservationScopes
   scope :recent, order('start_date, due_date, reserver_id')
   scope :user_sort, order('reserver_id')
-  scope :reserved, lambda { where("checked_out IS NULL and checked_in IS NULL and due_date >= ? and (approval_status = ? or approval_status = ?)", Time.now.midnight.utc, 'auto', 'approved').recent}
-  scope :checked_out, lambda { where("checked_out IS NOT NULL and checked_in IS NULL and due_date >=  ?", Time.now.midnight.utc).recent }
-  scope :checked_out_today, lambda { where("checked_out >= ? and checked_in IS NULL", Time.now.midnight.utc).recent } # shouldn't this just check checked_out = today?
-  scope :checked_out_previous, lambda { where("checked_out < ? and checked_in IS NULL and due_date <= ?", Time.now.midnight.utc, Date.tomorrow.midnight.utc).recent }
-  scope :overdue, lambda { where("checked_out IS NOT NULL and checked_in IS NULL and due_date < ?", Time.now.midnight.utc ).recent }
+  scope :finalized, where("approval_status = ? OR approval_status - ?",'auto','approved')
+  scope :not_returned, where("checked_in IS NULL").finalized.recent # called in the equipment_model model
+  scope :untouched, where("checked_out IS NULL").not_returned
+
+  scope :reserved, lambda { where("due_date >= ?", Time.now.midnight.utc).untouched.recent }
+  scope :checked_out, lambda { where("checked_out IS NOT NULL and due_date >=  ?", Time.now.midnight.utc).not_returned.recent }
+  scope :checked_out_today, lambda { where("checked_out >= ? and checked_out <= ?", Date.today.to_datetime, Date.tomorrow.to_datetime).not_returned.recent }
+  scope :checked_out_previous, lambda { where("checked_out < ? and due_date <= ?", Time.now.midnight.utc, Date.tomorrow.midnight.utc).not_returned.recent }
+  scope :overdue, lambda { where("due_date < ?", Time.now.midnight.utc ).checked_out.recent }
   scope :returned, where("checked_in IS NOT NULL and checked_out IS NOT NULL")
-  scope :returned_on_time, where("checked_in IS NOT NULL and checked_out IS NOT NULL and due_date >= checked_in").recent
-  scope :returned_overdue, where("checked_in IS NOT NULL and checked_out IS NOT NULL and due_date < checked_in").recent
-  scope :not_returned, where("checked_in IS NULL and (approval_status = ? or approval_status = ?)", 'auto', 'approved').recent # called in the equipment_model model
-  scope :missed, lambda {where("checked_out IS NULL and checked_in IS NULL and due_date < ? and (approval_status = ? OR approval_status = ?)", Time.now.midnight.utc, 'auto', 'approved').recent}
-  scope :upcoming, lambda {where("checked_out IS NULL and checked_in IS NULL and start_date = ? and due_date > ? and (approval_status = ? or approval_status = ?)", Time.now.midnight.utc, Time.now.midnight.utc, 'auto', 'approved').user_sort }
+  scope :returned_on_time, where("due_date >= checked_in").returned.recent
+  scope :returned_overdue, where("due_date < checked_in").returned.recent
+  scope :missed, lambda {where("due_date < ?", Time.now.midnight.utc).untouched.recent}
+  scope :upcoming, lambda {where("start_date = ?",Time.now.midnight.utc).reserved.user_sort }
   scope :starts_on_days, lambda {|start_date, end_date|  where(start_date: start_date..end_date)}
-  scope :reserved_on_date, lambda {|date|  where("start_date <= ? and due_date >= ? and (approval_status = ? or approval_status = ?)", date.to_time.utc, date.to_time.utc, 'auto', 'approved')}
-  scope :for_eq_model, lambda { |eq_model| where(equipment_model_id: eq_model.id) } # by default includes all reservations ever. limit e.g. checked_out via other scopes
-  scope :active, where("checked_in IS NULL and (approval_status = ? OR approval_status = ?)", 'auto', 'approved') # anything that's been reserved but not returned (i.e. pending, checked out, or overdue)
+  scope :reserved_on_date, lambda {|date|  where("start_date <= ? and due_date >= ?", date.to_time.utc, date.to_time.utc).finalized}
+  scope :for_eq_model, lambda { |eq_model| where(equipment_model_id: eq_model.id).finalized }
+  scope :active, not_returned
   scope :active_or_requested, lambda {where("checked_in IS NULL and approval_status != ?", 'denied')}
   scope :notes_unsent, where(notes_unsent: true)
   scope :requested, lambda {where("start_date >= ? and approval_status = ?", Time.now.midnight.utc, 'requested')}
@@ -24,8 +27,7 @@ module ReservationScopes
   scope :missed_requests, lambda {where("approval_status = ? and start_date < ?", 'requested', Time.now.midnight.utc)}
 
   scope :for_reserver, lambda { |reserver| where(reserver_id: reserver) }
-  scope :reserved_in_date_range, lambda { |start_date, end_date|
-    where("start_date < ? and due_date > ? and (approval_status = ? or approval_status = ?)", end_date, start_date, 'auto', 'approved') }
+  scope :reserved_in_date_range, lambda { |start_date, end_date| where("start_date < ? and due_date > ?)", end_date, start_date).finalized }
   scope :overlaps_with_date, lambda{ |date| where("start_date <= ? and due_date >= ?",date.to_datetime,date.to_datetime) }
 end
 
