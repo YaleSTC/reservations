e3d4d32f55daad7904efadd21579c850c1c3823b
squidgetx <squidgetx@gmail.com>
2014-07-21 12:30:07 -0400

    comment out spork blocks'

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,11 +4,11 @@ CodeClimate::TestReporter.start
 # CI build (repo token is encrypted in .travis.yml)
 
 require 'rubygems'
-require 'spork'
+#require 'spork'
 #uncomment the following line to use spork with the debugger
 #require 'spork/ext/ruby-debug'
 
-Spork.prefork do
+#Spork.prefork do
   # Loading more in this block will cause your tests to run faster. However,
   # if you change any configuration or code from libraries loaded here, you'll
   # need to restart spork for it take effect.
@@ -70,12 +70,12 @@ Spork.prefork do
     end
   end
 
-end
+#end
 
-Spork.each_run do
+#Spork.each_run do
   # This code will be run each time you run your specs.
 
-end
+#end
 
 # --- Instructions ---
 # Sort the contents of this file into a Spork.prefork and a Spork.each_run
