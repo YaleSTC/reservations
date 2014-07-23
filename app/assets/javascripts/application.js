0a4f12138a61f4afe783d0eff9980d0812ee19ad
squidgetx <squidgetx@gmail.com>
2014-07-23 17:29:43 -0400

    fix calendar in reports

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -304,19 +304,33 @@ function load_datepicker() {
       }
       $('.date_end').datepicker( "option" , "minDate" , start_date);
     }
-});
+  });
 
   $('.date_start_no_min').datepicker({
     altField: '#date_start_alt',
     altFormat: 'yy-mm-dd',
     onClose: function(dateText, inst) {
       var start_date = $('.date_start_no_min').datepicker("getDate");
-      var end_date = $('.date_end').datepicker("getDate");
+      var end_date = $('.date_end_no_min').datepicker("getDate");
       if (start_date > end_date){
         var new_date = new Date(start_date.getTime()+86400000);
-        $('.date_end').datepicker("setDate", new_date);
+        $('.date_end_no_min').datepicker("setDate", new_date);
       }
-      $('.date_end').datepicker( "option" , "minDate" , start_date);
+      $('.date_end_no_min').datepicker( "option" , "minDate" , start_date);
+    }
+  });
+
+  $('.date_end_no_min').datepicker({
+    altField: '#date_end_alt',
+    altFormat: 'yy-mm-dd',
+    onClose: function(dateText, inst) {
+      var start_date = $('.date_start_no_min').datepicker("getDate");
+      var end_date = $('.date_end_no_min').datepicker("getDate");
+      if (start_date > end_date) {
+        var new_date = new Date(start_date.getTime()+86400000);
+        $('.date_end_no_min').datepicker("setDate", new_date);
+      }
+      $('.date_end_no_min').datepicker( "option" , "minDate" , start_date);
     }
   });
 };
