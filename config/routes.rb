cd237ee8e972c39ac1e2b0a1d8850b8f9e041875
squidgetx <squidgetx@gmail.com>
2014-07-23 17:49:15 -0400

    add route edit

--- config/routes.rb
+++ config/routes.rb
@@ -92,7 +92,7 @@ Reservations::Application.routes.draw do
   get '/reports/:id/for_model' => 'reports#for_model', :as => :for_model_report
 
   get '/reports/for_model_set' => 'reports#for_model_set', :as => :for_model_set_reports # what http request? old match
-  get '/reports/update' => 'reports#update_dates', :as => :update_dates # what http request? old match
+  post '/reports/update' => 'reports#update_dates', :as => :update_dates # what http request? old match
   post '/reports/generate' => 'reports#generate', :as => :generate_report
 
   get '/logout' => 'application#logout', :as => :logout # what kind of http request is this? old match
