a4eecf92b840a271344cdeac14ad6f4574c6d01e
squidgetx <squidgetx@gmail.com>
2014-07-26 16:53:15 -0400

    appconfigs controller

--- app/models/app_config.rb
+++ app/models/app_config.rb
@@ -1,16 +1,5 @@
 class AppConfig < ActiveRecord::Base
 
- # attr_accessible :site_title, :admin_email, :department_name, :contact_link_location,
-  #                :home_link_text, :home_link_location,
-   #               :upcoming_checkin_email_body, :upcoming_checkin_email_active,
-    #              :overdue_checkin_email_body, :overdue_checkin_email_active,
-     #             :reservation_confirmation_email_active,
-      #            :delete_missed_reservations, :send_notifications_for_deleted_missed_reservations,
-       #           :deleted_missed_reservation_email_body,
-        #          :default_per_cat_page, :terms_of_service, :favicon,
-         #         :checkout_persons_can_edit, :override_on_create,
-          #        :override_at_checkout, :require_phone
-
   has_attached_file :favicon, url: "/system/:attachment/:id/:style/favicon.:extension"
 
   validates_with AttachmentContentTypeValidator, attributes: :favicon, content_type: 'image/vnd.microsoft.icon',
