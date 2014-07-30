3bfac24fcca1453b4189ee70a3bff93ebb492fb8
Oren Kanner <okanner@gmail.com>
2014-07-30 02:13:46 -0400

    added test db line to spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,6 +17,7 @@ require 'paper_trail'
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
+ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   # ## Mock Framework
