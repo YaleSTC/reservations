e82c7d2453eafd507354d5d2f358c5db508b0316
Qingyang Chen <qingyang.chen@gmail.com>
2014-09-16 18:32:06 -0400

    deleted config variable setting

--- db/migrate/20140909230819_set_sane_config_defaults.rb
+++ db/migrate/20140909230819_set_sane_config_defaults.rb
@@ -1,6 +1,5 @@
 class SetSaneConfigDefaults < ActiveRecord::Migration
   def change
-    config = AppConfig.first
     columns(:app_configs).each do |col|
       if (col.type == :string || col.type == :text) && !(col.name.include? 'favicon')
         # prevent nil strings/text and replace with empty string
