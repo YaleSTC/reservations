f7add8b0a6f8820cd3346a2fb14edfa7321c4129
Oren Kanner <okanner@gmail.com>
2014-09-17 19:21:08 -0400

    added cancancan authentication to rails admin initializer

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -9,7 +9,8 @@ RailsAdmin.config do |config|
   # config.current_user_method(&:current_user)
 
   ## == Cancan ==
-  # config.authorize_with :cancan
+  config.authorize_with :cancancan
+  config.current_user_method &:current_user
 
   ## == PaperTrail ==
   config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
