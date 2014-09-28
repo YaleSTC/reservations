84ababe5c99507c5ef0106dd8dd5a8fb6f9f4712
Oren Kanner <okanner@gmail.com>
2014-09-28 14:06:42 -0400

    commented out CAS dependency iin environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -5,8 +5,8 @@ require 'rails_extensions'
 # Version variable
 APP_VERSION = `git describe --tags --abbrev=0`.strip unless defined? APP_VERSION
 
-CASClient::Frameworks::Rails::Filter.configure(
-  :cas_base_url => "https://secure.its.yale.edu/cas/"
-)
+# CASClient::Frameworks::Rails::Filter.configure(
+#   :cas_base_url => "https://secure.its.yale.edu/cas/"
+# )
 # Initialize the rails application
 Reservations::Application.initialize!
