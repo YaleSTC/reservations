26dece6a4c0e5f3cbd0d734ca048ca3b590f008f
Oren Kanner <okanner@gmail.com>
2014-10-02 22:24:37 -0400

    added explanatory comment to setup_mail.rb

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -12,4 +12,6 @@ ActionMailer::Base.smtp_settings = {
 
 }
 
+# mailer host: replace "0.0.0.0:3000" with your root url in production
+# this will allow links in e-mail text to function correctly
 ActionMailer::Base.default_url_options[:host] = "0.0.0.0:3000"
