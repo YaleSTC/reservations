1932cf237e6a8abd512d75a8958c1f7b27faf54a
Oren Kanner <okanner@gmail.com>
2014-10-08 11:48:33 -0400

    updated app name and repo url in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid only for Capistrano 3.1
 lock '3.2.1'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'Reservations'
+set :repo_url, 'https://github.com/YaleSTC/reservations.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
