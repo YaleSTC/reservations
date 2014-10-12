53bf71cbd4c0d6cfd3f0565daba60413efd622c1
Oren Kanner <okanner@gmail.com>
2014-10-12 10:44:50 -0400

    fixed email config not to crash when table doesn't exist

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,7 +10,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = AppConfig.first.admin_email
+  config.mailer_sender = AppConfig.table_exists? && !AppConfig.first.nil? ? AppConfig.first.admin_email : 'admin@example.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
