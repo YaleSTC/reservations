36e1719fd4de6a1fe693423e46eb353dbe0e2cee
Oren Kanner <okanner@gmail.com>
2014-10-14 21:15:18 -0400

    added markdown link to equipment object creation notes

--- app/controllers/equipment_objects_controller.rb
+++ app/controllers/equipment_objects_controller.rb
@@ -35,7 +35,7 @@ class EquipmentObjectsController < ApplicationController
 
   def create
     @equipment_object = EquipmentObject.new(equipment_object_params)
-    @equipment_object.notes = "#### Created at #{Time.current.to_s(:long)} by #{current_user.name}"
+    @equipment_object.notes = "#### Created at #{Time.current.to_s(:long)} by #{current_user.md_link}"
     if @equipment_object.save
       flash[:notice] = "Successfully created equipment object. #{@equipment_object.serial}"
       redirect_to @equipment_object.equipment_model
