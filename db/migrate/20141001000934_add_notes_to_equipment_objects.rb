2130652b91fb428faee6e08ec47b2d3af628acd8
Oren Kanner <okanner@gmail.com>
2014-10-14 22:58:53 -0400

    added notes in migration for future work

--- db/migrate/20141001000934_add_notes_to_equipment_objects.rb
+++ db/migrate/20141001000934_add_notes_to_equipment_objects.rb
@@ -1,5 +1,8 @@
 class AddNotesToEquipmentObjects < ActiveRecord::Migration
   def change
     add_column :equipment_objects, :notes, :text, limit: 16777215, null: false
+
+    # Add code here to go through all existing equipment objects, their reservations in sequential order, and generate notes for checkin and checkout for all old reservations
+    # potentially make the `make_reservation_notes` method take the time/date as a parameter so that we can use it here.
   end
 end
