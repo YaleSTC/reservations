19b4e5bf57e06c4ce36592dfa5fb36a13b5c64d4
Oren Kanner <okanner@gmail.com>
2014-10-19 15:48:26 -0400

    only add deactivation_reason param if it isn't null

--- app/assets/javascripts/equipment_objects.js
+++ app/assets/javascripts/equipment_objects.js
@@ -1,5 +1,7 @@
 function handleDeactivation(e) {
   var p = prompt("Write down the reason for deactivation of this equipment object.")
-  e.href += "?deactivation_reason=" + encodeURIComponent(p)
+  if (p != null) {
+    e.href += "?deactivation_reason=" + encodeURIComponent(p)
+  }
 };
 
