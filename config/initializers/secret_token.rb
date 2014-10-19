dd31586b5e2eb5fcaa101cf52d86119847cc9fdc
Oren Kanner <okanner@gmail.com>
2014-10-19 18:45:30 -0400

    moved secret_token / secret_key_base to secrets.yml

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Reservations::Application.config.secret_token = 'ca480fdf45d4b289786275d6495e76324cd34a5f3cad9f488703a9974737cc339969c6abb788595ae4ec11f0894fc711dd1a4bf58f5bc71e77ca1ac10b4aa96c'
