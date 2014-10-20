57edf796ead3531e34a3f1382212ac7c082b04ec
Oren Kanner <okanner@gmail.com>
2014-10-20 00:12:53 -0400

    added current_password to strong params for update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -184,8 +184,9 @@ class UsersController < ApplicationController
 
   def user_params
     permitted_attributes = [:first_name, :last_name, :nickname, :phone,
-      :email, :affiliation, :terms_of_service_accepted, :created_by_admin,]
-    permitted_attributes += [:password, :password_confirmation] unless @cas_auth
+      :email, :affiliation, :terms_of_service_accepted, :created_by_admin]
+    permitted_attributes += [:password, :password_confirmation,
+      :current_password] unless @cas_auth
     permitted_attributes << :username if (can? :manage, Reservation)
     permitted_attributes += [:requirement_ids, :user_ids, :role] if can? :assign, :requirements
     p = params.require(:user).permit(*permitted_attributes)
