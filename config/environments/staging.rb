275e23d5366bf38b4fb57c55514bdb72450fddec
Oren Kanner <okanner@gmail.com>
2014-10-27 16:58:40 -0400

    added email link hostname config to staging.rb

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -3,5 +3,6 @@ require File.expand_path('../production', __FILE__)
 Reservations::Application.configure do
   #config.serve_static_assets = true
   config.action_mailer.perform_deliveries = false
+  config.action_mailer.default_url_options = { host: "example.com" }
 
 end
