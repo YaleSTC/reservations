f1ee3d23c5234f85a08c9e5725b7f982e929d492
Oren Kanner <okanner@gmail.com>
2014-10-26 22:28:03 -0400

    remove trackable

--- db/schema.rb
+++ db/schema.rb
@@ -195,11 +195,6 @@ ActiveRecord::Schema.define(version: 20141026050048) do
     t.string   "role",                                  default: "normal"
     t.boolean  "missing_phone",                         default: false
     t.string   "encrypted_password",        limit: 128, default: "",       null: false
-    t.integer  "sign_in_count",                         default: 0,        null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
   end
