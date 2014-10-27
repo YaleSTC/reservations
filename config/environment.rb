5f0b106d257a5b9823df988d5745dce6097dd257
Oren Kanner <okanner@gmail.com>
2014-10-27 11:32:43 -0400

    removed old CAS client configuration

--- config/environment.rb
+++ config/environment.rb
@@ -5,8 +5,5 @@ require 'rails_extensions'
 # Version variable
 APP_VERSION = `git describe --tags --abbrev=0`.strip unless defined? APP_VERSION
 
-# CASClient::Frameworks::Rails::Filter.configure(
-#   :cas_base_url => "https://secure.its.yale.edu/cas/"
-# )
 # Initialize the rails application
 Reservations::Application.initialize!
