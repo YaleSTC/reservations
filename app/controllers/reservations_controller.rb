50ec2c551d2e3bdf638246cdc3ccca2bbe355045
Oren Kanner <okanner@gmail.com>
2014-10-29 20:30:33 -0400

    improved handling of archive prompt

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -384,6 +384,9 @@ class ReservationsController < ApplicationController
     if params[:archive_note].nil? || params[:archive_note].strip.empty?
       flash[:error] = 'Reason for archiving cannot be empty.'
       redirect_to :back and return
+    elsif params[:archive_note] == 'null'
+      flash[:notice] = 'Reservation archiving cancelled.'
+      redirect_to :back and return
     end
     set_reservation
     if @reservation.checked_in
