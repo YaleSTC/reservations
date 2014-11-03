574feb0bbc15b1011f6ad5f2626ba3337cb0acb6
Oren Kanner <okanner@gmail.com>
2014-11-03 00:58:14 -0500

    fixed whitespace in warning

--- app/assets/javascripts/equipment_objects.js
+++ app/assets/javascripts/equipment_objects.js
@@ -1,6 +1,6 @@
 function handleDeactivation(e, overbooked) {
   if (arguments.length > 1 && overbooked.length > 0) {
-    var c = confirm("This equipment will be overbooked over the coming week from "+overbooked[0]+" to "+overbooked[overbooked.length-1]+". Are you sure you want to continue?")
+    var c = confirm("This equipment will be overbooked over the coming week from"+overbooked[0]+" to"+overbooked[overbooked.length-1]+". Are you sure you want to continue?")
   }
   else {
     var c = true
