62c8f4c955d30aed1a0b6673fd569cb7858d5496
Oren Kanner <okanner@gmail.com>
2014-11-03 00:47:12 -0500

    refactored javascript conditional

--- app/assets/javascripts/equipment_objects.js
+++ app/assets/javascripts/equipment_objects.js
@@ -1,6 +1,5 @@
 function handleDeactivation(e, overbooked) {
-  if (arguments.length == 1) { var overbooked = [] }
-  if (overbooked.length > 0) {
+  if (arguments.length > 1 && overbooked.length > 0) {
     var c = confirm("This equipment will be overbooked over the coming week from "+overbooked[0]+" to "+overbooked[overbooked.length-1]+". Are you sure you want to continue?")
   }
   else {
