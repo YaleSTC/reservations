e959830e0e98bd60d8e35e77af5bc97e9bdd9f88
Oren Kanner <okanner@gmail.com>
2014-11-04 00:01:13 -0500

    broke up strings for readability

--- app/assets/javascripts/equipment_objects.js
+++ app/assets/javascripts/equipment_objects.js
@@ -1,17 +1,22 @@
 function handleDeactivation(e, reservation_id, overbooked) {
   if (arguments.length > 1 && reservation_id) {
-    var confirmCheckedOut = confirm("This equipment is currently checked out. If you continue then reservation #"+reservation_id+" will be archived. Do you want to proceed?")
+    var confirmCheckedOut = confirm("This equipment is currently checked " +
+      "out. If you continue then reservation #"+reservation_id+" will be " +
+      "archived. Do you want to proceed?")
   } else {
     var confirmCheckedOut = true
   }
   if (confirmCheckedOut == true && arguments.length > 2 && overbooked.length > 0) {
-    var confirmOverbooked = confirm("This equipment will be overbooked over the coming week from"+overbooked[0]+" to"+overbooked[overbooked.length-1]+". Are you sure you want to continue?")
+    var confirmOverbooked = confirm("This equipment will be overbooked " +
+      "over the coming week from"+overbooked[0]+"to" +
+      overbooked[overbooked.length-1]+". Are you sure you want to continue?")
   }
   else {
     var confirmOverbooked = true
   }
   if (confirmCheckedOut == true && confirmOverbooked == true) {
-    var reason = prompt("Write down the reason for deactivation of this equipment object.")
+    var reason = prompt("Write down the reason for deactivation of this " +
+      "equipment object.")
     if (reason == null) {
       e.href += "?deactivation_cancelled=1"
     } else if (reason != "") {
