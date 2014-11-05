f83dec3153d5ae533dcdf2c2016ef24d2464bb6e
Oren Kanner <okanner@gmail.com>
2014-11-04 20:09:49 -0500

    updated migration to generate history for existing equipment objects

--- db/migrate/20141001000934_add_notes_to_equipment_objects.rb
+++ db/migrate/20141001000934_add_notes_to_equipment_objects.rb
@@ -2,7 +2,16 @@ class AddNotesToEquipmentObjects < ActiveRecord::Migration
   def change
     add_column :equipment_objects, :notes, :text, limit: 16777215, null: false
 
-    # Add code here to go through all existing equipment objects, their reservations in sequential order, and generate notes for checkin and checkout for all old reservations
-    # potentially make the `make_reservation_notes` method take the time/date as a parameter so that we can use it here.
+    # go through all existing equipment objects, including reservations and
+    # associated users
+    EquipmentObject.includes(reservations: [:reserver, :checkout_handler, :checkin_handler]).all.each do |eo|
+      # add creation note
+      eo.update_attributes(notes: "#### Created at #{eo.created_at.to_s(:long)}")
+      # go through all reservations and make notes
+      eo.reservations.sort_by(&:checked_out).each do |res|
+        eo.make_reservation_notes('checked_out', res, res.checkout_handler, '', res.checked_out)
+        eo.make_reservation_notes('checked_in', res, res.checkin_handler, '', res.checked_in) if res.checked_in
+      end
+    end
   end
 end
