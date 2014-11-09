80825f8a02086d9cb712677762ead5a17040b224
aaron-effron <aaron.effron@yale.edu>
2014-11-09 09:55:37 -0500

    Update reservations.js
    Made more descriptive variable name for archive reason, using camel case.

--- app/assets/javascripts/reservations.js
+++ app/assets/javascripts/reservations.js
@@ -1,4 +1,4 @@
 function getArchiveNote(e) {
-  var p = prompt("Write down the reason for archiving this reservation.")
-  e.href += "?archive_note=" + encodeURIComponent(p)
+  var archiveReason = prompt("Write down the reason for archiving this reservation.")
+  e.href += "?archive_note=" + encodeURIComponent(archiveReason)
 };
