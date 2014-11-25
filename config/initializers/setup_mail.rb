29bb293155df0da3f06975236356d96d01f1b9bc
Oren Kanner <okanner@gmail.com>
2014-11-25 03:29:57 -0500

    fixed broken email initializer

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -4,12 +4,16 @@ ActionMailer::Base.smtp_settings = {
   :address              => Rails.application.secrets.smtp_address,
   :port                 => Rails.application.secrets.smtp_port,
   :domain               => Rails.application.secrets.smtp_domain,
-  # with these disabled, the server must be connected to the yale network for email to work
-  if ENV['RES_SMTP_AUTHENTICATION']
-    :authentication      => :login,
-    :user_name           => Rails.application.secrets.smtp_username,
-    :password            => Rails.application.secrets.smtp_password,
-  end
   :enable_starttls_auto => true
 
 }
+
+# with these disabled, the server must be connected to the yale network for
+# email to work
+if ENV['RES_SMTP_AUTHENTICATION']
+  ActionMailer::Base.smtp_settings[:authentication] = :login
+  ActionMailer::Base.smtp_settings[:user_name] =
+    Rails.application.secrets.smtp_username
+  ActionMailer::Base.smtp_settings[:password] =
+    Rails.application.secrets.smtp_password
+end
