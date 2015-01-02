9dce6f8978491be865935fedaae7d4fa9d64277a
Oren Kanner <okanner@gmail.com>
2015-01-01 20:47:20 -0500

    cleaned up CartValidations model

--- app/models/cart_validations.rb
+++ app/models/cart_validations.rb
@@ -3,7 +3,7 @@ module CartValidations
   # queries as possible. Often it seems that a scope could be used but
   # it is important to remember that Rails lazy-loads the database calls
   #
-  def validate_all(renew = false)
+  def validate_all(renew = false) # rubocop:disable AbcSize
     # 2 queries for every equipment model in the cart because of num_available
     # plus about 8 extra
     #
@@ -20,7 +20,10 @@ module CartValidations
 
     errors += check_requirements(models)
 
-    source_res = Reservation.not_returned.where(equipment_model_id: items.keys).reserved_in_date_range(start_date, due_date).all
+    source_res =
+      Reservation.not_returned
+      .where(equipment_model_id: items.keys)
+      .reserved_in_date_range(start_date, due_date).all
 
     models.each do |model, quantity|
       errors += check_availability(model, quantity, source_res)
@@ -35,7 +38,9 @@ module CartValidations
     # 1 query
     errors = []
     if Blackout.hard.for_date(start_date).count > 0
-      errors << "#{Blackout.get_notices_for_date(start_date, :hard)} (a reservation cannot start on #{start_date.to_date.strftime('%m/%d')})"
+      errors << "#{Blackout.get_notices_for_date(start_date, :hard)} "\
+        '(a reservation cannot start on '\
+        "#{start_date.to_date.strftime('%m/%d')})"
     end
     errors
   end
@@ -45,7 +50,9 @@ module CartValidations
     # 1 query
     errors = []
     if Blackout.hard.for_date(due_date).count > 0
-      errors << "#{Blackout.get_notices_for_date(due_date, :hard)} (a reservation cannot end on #{due_date.to_date.strftime('%m/%d')})"
+      errors << "#{Blackout.get_notices_for_date(due_date, :hard)} "\
+        '(a reservation cannot end on '\
+        "#{due_date.to_date.strftime('%m/%d')})"
     end
     errors
   end
@@ -55,12 +62,13 @@ module CartValidations
     # 1 query
     errors = []
     if Reservation.for_reserver(reserver_id).overdue.count > 0
-      errors << 'This user has overdue reservations that prevent him/her from creating new ones'
+      errors << 'This user has overdue reservations that prevent him/her '\
+        'from creating new ones'
     end
     errors
   end
 
-  def check_max_items
+  def check_max_items # rubocop:disable MethodLength, AbcSize
     # check that the cart items would not cause the reserver to have
     # more than the max allowed number of the same equipment model
     # or the max allowed number of the same category item
@@ -68,7 +76,8 @@ module CartValidations
     #
     # 4 queries
     errors = []
-    relevant = Reservation.for_reserver(reserver_id).not_returned.includes(:equipment_model).all
+    relevant = Reservation.for_reserver(reserver_id).not_returned
+               .includes(:equipment_model).all
     category = {}
 
     # get hash of model objects and quantities
@@ -82,10 +91,12 @@ module CartValidations
       max_models = model.maximum_per_user
 
       start_date.to_date.upto(due_date.to_date) do |d|
-        if Reservation.number_for_model_on_date(d, model.id, relevant) + quantity > max_models
-          errors << "Only #{max_models} #{model.name.pluralize} can be reserved at a time"
-          break
-        end
+        next unless Reservation.number_for_model_on_date(d, model.id,
+                                                         relevant)\
+          + quantity > max_models
+        errors << "Only #{max_models} #{model.name.pluralize} can be "\
+          'reserved at a time.'
+        break
       end
       if category.include?(model.category)
         category[model.category] += quantity
@@ -97,10 +108,12 @@ module CartValidations
     category.each do |cat, q|
       max_cat = cat.maximum_per_user
       start_date.to_date.upto(due_date.to_date) do |d|
-        if Reservation.number_for_category_on_date(d, cat.id, relevant) + q > max_cat
-          errors << "Only #{max_cat} #{cat.name.pluralize} can be reserved at a time"
-          break
-        end
+        next unless Reservation.number_for_category_on_date(d, cat.id,
+                                                            relevant)\
+          + q > max_cat
+        errors << "Only #{max_cat} #{cat.name.pluralize} "\
+          'can be reserved at a time.'
+        break
       end
     end
 
@@ -109,28 +122,31 @@ module CartValidations
 
   def check_availability(model = EquipmentModel.find(items.keys.first),
                          quantity = 1,
-                         source_res = Reservation.for_eq_model(self).not_returned.all)
+                         source_res = Reservation.for_eq_model(self)
+                           .not_returned.all)
 
-    # checks that the model is available for the given quantity
-    # given the existence of the source_reservations
+    # checks that the model is available for the given quantity given the
+    # existence of the source_reservations
     #
-    # if called with no arguments, automatically assumes it is
-    # checking for a cart with only 1 argument with quantity 1
-    # which is a common case when checking availability of a single
-    # reservation (eg, reservation.to_cart.check_availability)
+    # if called with no arguments, automatically assumes it is checking for
+    # a cart with only 1 argument with quantity 1 which is a common case when
+    # checking availability of a single reservation (eg,
+    # reservation.to_cart.check_availability)
     #
-    # to check the contents of a whole cart you need to iterate over
-    # its items hash to pass in the models and quantities
+    # to check the contents of a whole cart you need to iterate over its
+    # items hash to pass in the models and quantities
     #
-    # the advantage is that source_res is constant and requires no
-    # additional DB calls. unfortunately num-available still requires
-    # 2 queries to establish the max number of available items
+    # the advantage is that source_res is constant and requires no additional
+    # DB calls. unfortunately num-available still requires 2 queries to
+    # establish the max number of available items
     #
     # 2 queries
 
     errors = []
-    if model.num_available_from_source(start_date, due_date, source_res) < quantity
-      errors << "That many #{model.name.pluralize} are not available for the given time range"
+    if model.num_available_from_source(start_date, due_date,
+                                       source_res) < quantity
+      errors << "That many #{model.name.pluralize} are not available for "\
+        'the given time range.'
     end
     errors
   end
@@ -145,12 +161,14 @@ module CartValidations
     errors = []
     max_length = model.maximum_checkout_length
     if duration > max_length
-      errors << "#{model.name.titleize} can only be reserved for #{max_length} days"
+      errors << "#{model.name.titleize} can only be reserved for "\
+        "#{max_length} days"
     end
     errors
   end
 
-  def check_should_be_renewed(user_reservations = Reservations.for_reserver(reserver).not_returned,
+  def check_should_be_renewed(user_reservations = Reservations
+    .for_reserver(reserver).not_returned,
                               model = EquipmentModel.find(items.keys.first),
                               start_date = self.start_date)
     # if the user reservations array has a reservation for the model that
@@ -161,9 +179,11 @@ module CartValidations
     # 0 queries, except for is_eligible_for_renew
     errors = []
     user_reservations.each do |r|
-      if r.equipment_model_id == model.id && r.due_date == start_date && r.is_eligible_for_renew?
-        errors << "#{model.name.titleize} should be renewed instead of re-checked out"
-      end
+      next unless r.equipment_model_id == model.id &&
+                  r.due_date == start_date &&
+                  r.is_eligible_for_renew?
+      errors << "#{model.name.titleize} should be renewed instead of "\
+          're-checked out.'
     end
     errors
   end
@@ -185,6 +205,7 @@ module CartValidations
     unfulfilled_reqs.each do |r|
       unfulfilled_req_text << r.description
     end
-    ["#{user.name} is missing the following certifications: #{unfulfilled_req_text.to_sentence}"]
+    ["#{user.name} is missing the following certifications: "\
+      "#{unfulfilled_req_text.to_sentence}"]
   end
 end
