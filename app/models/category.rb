e49ec483c930ea92ecf8376da4c2442a238a8187
Oren Kanner <okanner@gmail.com>
2015-01-01 20:58:27 -0500

    cleaned up Category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,8 +4,7 @@ class Category < ActiveRecord::Base
 
   has_many :equipment_models, dependent: :destroy
 
-  validates :name,                presence: true,
-                                  uniqueness: true
+  validates :name, presence: true, uniqueness: true
 
   validates :max_per_user,
             :max_checkout_length,
@@ -20,8 +19,9 @@ class Category < ActiveRecord::Base
 
   nilify_blanks only: [:deleted_at]
 
-  # table_name is needed to resolve ambiguity for certain queries with 'includes'
-  scope :active, lambda { where("#{table_name}.deleted_at is null") }
+  # table_name is needed to resolve ambiguity for certain queries with
+  # 'includes'
+  scope :active, ->() { where("#{table_name}.deleted_at is null") }
 
   def maximum_per_user
     max_per_user || Float::INFINITY
