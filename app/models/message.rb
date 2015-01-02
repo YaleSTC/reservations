0c32152cf734ef83d712e556db631fb9770e8abd
Oren Kanner <okanner@gmail.com>
2015-01-01 21:12:25 -0500

    cleaned up Message and Requirement models

--- app/models/message.rb
+++ app/models/message.rb
@@ -6,7 +6,7 @@ class Message
   attr_accessor :name, :email, :subject, :body
 
   validates :name, :email, :subject, :body, presence: true
-  validates :email, format: { with: %r{.+@.+\..+} }, allow_blank: true
+  validates :email, format: { with: /.+@.+\..+}/ }, allow_blank: true
 
   def initialize(attributes = {})
     attributes.each do |name, value|
