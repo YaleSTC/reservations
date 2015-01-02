94f0fb735d056a83c41b1dd0c8d6f347a8baed38
Oren Kanner <okanner@gmail.com>
2015-01-02 10:54:16 -0500

    cleaned up remaining config files

--- config/application.rb
+++ config/application.rb
@@ -8,38 +8,46 @@ Bundler.require(:default, Rails.env)
 
 module Reservations
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
+    # Settings in config/environments/* take precedence over those specified
+    # here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
     config.autoload_paths += %W(#{config.root}/lib/extras #{config.root}/lib/)
     config.watchable_dirs['lib'] = [:rb]
-    # Only load the plugins named here, in the order given (default is alphabetical).
+    # Only load the plugins named here, in the order given (default is
+    # alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
 
     # Activate observers that should always be running.
-    # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
+    # config.active_record.observers = :cacher, :garbage_collector,
+    #                                  :forum_observer
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # Set Time.zone default to the specified zone and make Active Record
+    # auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names.
+    # Default is UTC.
     config.time_zone = 'Eastern Time (US & Canada)'
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # The default locale is :en and all translations from
+    # config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path +=
+    #   Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # JavaScript files you want as :defaults (application.js is always included).
+    # JavaScript files you want as :defaults (application.js is always
+    # included).
     # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
 
     # Configure the default encoding used in templates for Ruby 1.9.
-    config.encoding = "utf-8"
+    config.encoding = 'utf-8'
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
-    #config.rubycas.cas_base_url = 'https://secure.its.yale.edu/cas/'
+    # config.rubycas.cas_base_url = 'https://secure.its.yale.edu/cas/'
 
     # Enable the asset pipeline
     config.assets.enabled = true
@@ -51,12 +59,11 @@ module Reservations
     # config.assets.prefix = "/assets"
 
     # Add app/assets/fonts to the asset pipeline known paths
-    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
 
     # Ensures that each controller doesn't load every helper in the application
     config.action_controller.include_all_helpers = false
 
     PermanentRecords.dependent_record_window = 10.seconds
-
   end
 end
