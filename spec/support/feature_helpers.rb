47c7f85d632e6e6c43c51d3efbc71e3bbfab995a
Oren Kanner <okanner@gmail.com>
2015-01-02 01:57:28 -0500

    cleaned up spec support

--- spec/support/feature_helpers.rb
+++ spec/support/feature_helpers.rb
@@ -2,13 +2,15 @@ module FeatureHelpers
   # make sure we have a working app
   def app_setup
     @app_config = FactoryGirl.create(:app_config)
-    @equipment_model_with_object = FactoryGirl.create(:equipment_model_with_object)
+    @equipment_model_with_object =
+      FactoryGirl.create(:equipment_model_with_object)
     @admin = FactoryGirl.create(:admin)
     @user = FactoryGirl.create(:user)
   end
 
   def fill_in_registration
-    fill_in 'Email', with: (0...8).map { (65 + rand(26)).chr }.join + '@example.com'
+    fill_in 'Email',
+            with: (0...8).map { (65 + rand(26)).chr }.join + '@example.com'
     fill_in 'user_password', with: 'passw0rd'
     fill_in 'user_password_confirmation', with: 'passw0rd'
     fill_in 'First name', with: 'John'
