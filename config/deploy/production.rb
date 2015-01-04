5d45f38f47aef00db5ac7eb3afe8bb11277a48c8
Oren Kanner <okanner@gmail.com>
2015-01-04 00:25:49 -0500

    cleaned up capistrano environment files

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,15 +8,13 @@
 # role :web, %w{deploy@example.com}
 # role :db,  %w{deploy@example.com}
 
-
 # Extended Server Syntax
 # ======================
 # This can be used to drop a more detailed server definition into the
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server 'localhost', user: 'hudson', roles: %w{web app db}
-
+server 'localhost', user: 'hudson', roles: %w(web app db)
 
 # Custom SSH Options
 # ==================
