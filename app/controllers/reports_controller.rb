e9e2248bb7a02f54409038da2a9a5d28434bbe01
squidgetx <squidgetx@gmail.com>
2015-01-06 15:26:02 -0500

    major refactor

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -19,134 +19,6 @@ class ReportsController < ApplicationController
                :"Missed" => :missed }
 
 
-  def average2 arr
-    if arr.size == 0
-      'N/A'
-    else
-      (arr.inject { |r, e| r + e }.to_f / arr.size).round(2)
-    end
-  end
-    
-  def avg_duration res_set
-    average2(res_set.collect { |r| r.duration })
-  end
-
-  def avg_time_out res_set
-    average2(res_set.collect { |r| r.time_checked_out })
-  end
-
-  class Reservation::ActiveRecord_Relation
-    include Rails.application.routes.url_helpers
-    
-    def reserver_row_builders
-      reservers = self.collect(&:reserver).uniq
-      reservers.collect do |user|
-        RowBuilder.new(user.name, :reserver_id, [user.id],
-                       user_path(id: user.id))
-      end
-      reservers
-    end
-
-    def equipment_row_builders
-      eq_model_info = []
-      cat_em_ids = {}
-      em_ids = self.collect(&:equipment_model_id).uniq
-      eq_models = EquipmentModel.includes(:category).find(em_ids)
-      eq_models.each do |em|
-        eq_model_info << RowBuilder.new(em.name, :equipment_model_id, [em.id],
-                                        for_model_report_path(id: em.id))
-        if cat_em_ids[em.category.id]
-          cat_em_ids[em.category.id] << em.id
-        else
-          cat_em_ids[em.category.id] = [em.id]
-        end
-      end
-      categories = Category.find(cat_em_ids.keys)
-      category_info = categories.collect do |cat|
-        RowBuilder.new(cat.name, :equipment_model_id, cat_em_ids[cat.id],
-                       for_model_set_reports_path(ids: cat_em_ids[cat.id]))
-      end
-      return eq_model_info, category_info
-    end
-
-    def build_relations_set
-      # reservation relations for each of the scopes
-      def_options = { stat_type: :count }
-      res_rels = []
-      SCOPES.each do |key, value|
-        rel = value ? self.send(value) : self
-        res_rels << ColumnBuilder.new(key.to_s, rel, def_options)
-      end
-      res_rels
-    end
-
-  end
- 
-  # The idea I had behind reports was to be able to have relatively flexible
-  # report building capabilities without a lot of queries
-  #
-  # build_table
-  # builds a table in rows where each row is a set of reservations and the
-  # relation filters the data again by column - it's useful for subgrouping
-  # the reservations, and params lets you alter what kind of data you're
-  # looking for
-  #
-  # assoc_details and collect_res_details take the set of
-  # reservations, and for each reservation collect details from associated
-  # tables, and the reservation table
-  #
-  # ColumnBuilder should really be renamed ColumnBuilder. It has a few fields:
-  # Title ; the text displayed at the head of the data column
-  # Relation ; the ActiveRecord::Relation instance of the reservations
-  #   that the column cares about
-  # Options ; a cheap trick to get in multiple arguments at the end here.
-  #   I want to rearchitect this to be stat_type instead. The logic will go,
-  #   'duration, time_out, count' are special keywords
-  #   any other word will assume it is counting the field
-  #
-  #
-  # Properties of a table; how best to organize them?
-  # (Full) Set of Reservations
-  #   - Criteria for filtering; eg category, EM, user, EO, etc.
-  # Column headers
-  #   - for each column, how to filter the reservations (relation)
-  #   - for each row, how to display the data
-  # Row headers
-  #   - what are the rows (id_type)
-  #   - for each row, what object is it (this is a full table metadata)
-  # So..
-  #
-  # Struct TableBuilder
-  #   - Name
-  #   - AR relation for 'all reservations'
-  #   - RowObject type
-  #   - Array of ColRelationDisplays
-  #   - Array of RowObjects
-  #
-  # Struct ColRelationDisplay (name subject to change)
-  #   - Name
-  #   - AR relation
-  #   - Display type
-  #
-  # Struct RowObject
-  #   - Name
-  #   - Link path
-  #   - Object ID
-  #
-  # Building the table
-  # 1x query to get the TableBuilder AR
-  # n-columns x queries to get ARs for each column
-  # that's it; when building rows use Array.select to match the ids
-  #
-  #  Struct Table
-  #   - Array of column headings (strings)
-  #   - Array of Rows
-  #  Struct Row
-  #   - Name
-  #   - Link path (these can be copied from Row Object)
-  #   - Array of data (strings or ints)
-  #
-
   def index # rubocop:disable MethodLength, AbcSize
     @res_stat_sets = []
     @start_date = start_date
@@ -155,29 +27,14 @@ class ReportsController < ApplicationController
     # filter reservations by date
     reservations = Reservation.starts_on_days(@start_date, @end_date)
                 .includes(:equipment_model)
-
-    # build ColumnBuilders array; these are the columns of the data table
-    columns = reservations.build_relations_set
-    columns << ColumnBuilder.new('User Count', reservations,
-                                stat_type: :count, 
-                                secondary_id: :reserver_id)
-
-    # Get RowBuilder array data from the reservation sets; these are 
-    # basically the rows for each data table 
-    equipment_row_builders = reservations.equipment_row_builders
-    eq_models = equipment_row_builders[0]
-    categories = equipment_row_builders[1]
-    #reserver_info = reservations.user_row_builders
-
-    # sets of reservations are passed in by name then models associated
-    all_models = [RowBuilder.new('All Models', :equipment_model_id)]
-
-    table_types = { total: all_models, #users: reserver_info,
-                  categories: categories, equipment_models: eq_models }
+    eq_models = Report.build_new("Equipment Models", :equipment_model_id,
+                                nil, reservations)
+    eq_models.populate_data
+    table_hash = {}
+    table_hash[:rows] = eq_models.rows
+    table_hash[:col_names] = eq_models.columns.collect { |c| c.name }
     @data_tables = {}
-    table_types.each do |name, rows|
-      @data_tables[name] = build_table(rows, columns)
-    end
+    @data_tables[eq_models.name] = table_hash
 
     respond_to do |format|
       format.html
---
+++ app/models/report.rb
@@ -0,0 +1,139 @@
+class Report
+  include Rails.application.routes.url_helpers
+  attr_accessor :name, :reservations,
+    :columns, :row_item_type, :rows
+  DEFAULT_COLUMNS = [ ['Total', :all, :count],
+                      ['Reserved', :reserved, :count],
+                      ['Checked Out', :checked_out, :count],
+                      ['Overdue', :overdue, :count],
+                      ['Returned On Time', :returned_on_time, :count],
+                      ['Returned Overdue', :returned_overdue, :count],
+                      ['User Count', :all, :reserver_id],
+  ]
+
+
+  class Column
+    attr_accessor :name, :res_set, :data_type, :filter
+    def self.arr_to_col arr
+      c = self.new
+      c.name = arr[0]
+      c.filter = arr[1]
+      c.data_type = arr[2]
+      c
+    end
+  end
+
+  class Row
+    attr_accessor :name, :link_path, :item_id, :data
+  end
+
+  # -- Private class helper methods -- #
+
+  def self.average2 arr
+    if arr.size == 0
+      'N/A'
+    else
+      (arr.inject { |r, e| r + e }.to_f / arr.size).round(2)
+    end
+  end
+    
+  def self.avg_duration res_set
+    average2(res_set.collect { |r| r.duration })
+  end
+
+  def self.avg_time_out res_set
+    average2(res_set.collect { |r| r.time_checked_out })
+  end
+
+  def self.count_unique(res_set, type)
+    # count the # of occurences of unique <type>s in the reservation set,
+    # eg how many unique reservers there are in the set
+    items = res_set.collect do |res|
+      res.send(type)
+    end
+    items.uniq.count
+  end
+
+  def self.calculate(res_set, type)
+    # given an array of reservations, calculate some data
+    case type
+    when :count
+      return res_set.count
+    when :duration
+      return avg_duration res_set
+    when :time_checked_out
+      return avg_time_out res_set
+    else
+      return count_unique(res_set, type)
+    end
+  end
+  
+  def self.get_class(symbol)
+    # convert a symbol id field to a class, eg :user_id -> User
+    symbol.to_s[0...-3].camelize.constantize
+  end
+
+  
+  # build the skeleton of the report.
+  # name is used for display, row_item_type must be a symbol
+  # of an ID field that the reservation can respond to (eg, :reserver_id)
+  # path_method is the method that should be used on the links
+  # reservations is the ActiveRecord::Relation of the reservations to consider
+  # columns is an array of 3 element arrays used to build the column objects
+  # this is so that code outside of the model can interface with this method
+  # without having to use a custom data structure
+
+  def self.build_new(name, row_item_type, path_method = nil,
+                     reservations = Reservation.all,
+                     columns = DEFAULT_COLUMNS)
+    report = self.new
+    report.name = name
+    report.reservations = reservations
+    report.row_item_type = row_item_type
+
+    # convert array of column attributes into column objects
+    report.columns = columns.collect do |col|
+      Column.arr_to_col col
+    end
+
+    # get the reservations for each column
+    report.columns.each do |col|
+      col.res_set = reservations.send(col.filter)
+    end
+
+    # set the row objects
+    item_ids = reservations.collect do |res|
+      res.send(row_item_type)
+    end
+    items = get_class(row_item_type).find(item_ids)
+    report.rows = items.collect do |item|
+      r = Row.new
+      r.name = item.name
+      r.item_id = item.id
+      r.link_path = send(path_method, id: r.item_id) if path_method
+      r
+    end
+
+    report
+  end
+    
+  # -- Instance methods -- #
+
+  def populate_data
+    # populate the reports' rows.data 
+    # iterate by row
+    @rows.each do |row|
+      row.data = []
+      @columns.each do |col|
+        # get all the reservations in the set that are relevant
+        # to the row object we're looking at
+        res_set = col.res_set.select do |res|
+          row.item_id == res.send(@row_item_type)
+        end
+        # add the datum to the array
+        row.data << Report.calculate(res_set, col.data_type)
+      end
+    end
+  end
+
+end
