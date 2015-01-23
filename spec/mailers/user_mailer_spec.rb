69a109544ae4dde06f4b3c73589f52b6c7c3f453
Alan Liu <AlanL.Liu@yahoo.com>
2015-01-23 17:41:13 -0500

    Added a link to the reservation in various emails
    Changed .nil? on notes to .blank?
    added markdown to the notes that were missing it
    (still need to test)
    
    Signed-off-by: Alan Liu <AlanL.Liu@yahoo.com>
    
    Added an absolute link to the reservations in checkin receipt, checkout receipt, checked_in_fine email, and reservation confirmation.
    
    Added tests to those four emails to see if they contain a link with reservations in it.
    
    fixed travis offenses
    
    removed extra spaces, reverted a few changes to requirements notes,
    
    improved the test by adding in the full url. added a custom test for one case because @res is an array
    
    travis
    
    travis try #2

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -15,6 +15,14 @@ shared_examples_for 'valid user email' do
   # end
 end
 
+shared_examples_for 'contains reservation' do
+  it 'has reservation link' do
+    # body contains link to the reservation
+    expect(@mail.body).to \
+      include("<a href=\"http://0.0.0.0:3000/reservations/#{@res.id}\"")
+  end
+end
+
 describe UserMailer, type: :mailer do
   before(:all) do
     @app_config = FactoryGirl.create(:app_config)
@@ -32,11 +40,11 @@ describe UserMailer, type: :mailer do
       @mail = UserMailer.checkin_receipt(@res).deliver
     end
     it_behaves_like 'valid user email'
+    it_behaves_like 'contains reservation'
     it 'includes overdue information when overdue' do
       @res.due_date = @res.checked_in.to_date - 1.day
       @res.save(validate: false)
       @mail = UserMailer.checkin_receipt(@res).deliver
-
       expect(@mail.body).to include('late fee')
     end
   end
@@ -47,6 +55,7 @@ describe UserMailer, type: :mailer do
       @mail = UserMailer.checkout_receipt(@res).deliver
     end
     it_behaves_like 'valid user email'
+    it_behaves_like 'contains reservation'
   end
   describe 'missed_reservation_notification' do
     before do
@@ -70,6 +79,7 @@ describe UserMailer, type: :mailer do
       @res.save(validate: false)
       @mail = UserMailer.overdue_checked_in_fine(@res).deliver
     end
+    it_behaves_like 'contains reservation'
     it_behaves_like 'valid user email'
   end
   describe 'overdue_checked_in_fine with no fine' do
@@ -89,6 +99,12 @@ describe UserMailer, type: :mailer do
     before do
       @res = [] << FactoryGirl.create(:valid_reservation, reserver: reserver)
       @mail = UserMailer.reservation_confirmation(@res).deliver
+    end
+    it 'has reservation link' do
+      # custom test because @res is an array
+      # body contains link to the reservation,
+      expect(@mail.body).to \
+        include("<a href=\"http://0.0.0.0:3000/reservations/#{@res[0].id}\"")
     end
     it_behaves_like 'valid user email'
   end
