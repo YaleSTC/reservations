514d75af69a1ac8fb92398e9b2f360f0ddc67e12
Machiste Quintana <machiste.quintana@yale.edu>
2015-01-28 15:26:59 -0500

    remove extraneous console.log [ci skip]

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -160,7 +160,6 @@ $(document).ready(function() {
 
   $('#modal').click(function() {
     $('#userModal div.modal-body').load(new_user, {possible_netid: $('#fake_reserver_id').val() }); // new_user defined in variables.js.erb
-    console.log(new_user);
   });
 
   load_datepicker();
