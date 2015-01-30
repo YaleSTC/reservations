349921898ba44aee34119b1160092dfbada93bc8
Machiste Quintana <machiste.quintana@yale.edu>
2015-01-30 09:18:41 -0500

    Revert "fix all the remaining btns [skip ci]"
    This reverts commit ea33f7bb84f99412926b3804a61cbf8e4e71a839.
    
    I did not do that find and replace right.

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -61,7 +61,7 @@ SimpleForm.setup do |config|
   config.boolean_style = :nested
 
   # Default class for buttons
-  config.button_class = btn btn-default
+  config.button_class = 'btn'
 
   # Method used to tidy up errors. Specify any Rails Array method.
   # :first lists the first message for each field.
