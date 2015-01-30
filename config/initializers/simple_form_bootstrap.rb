e6eaacf517930ba075c1c0dd7e17e2f6694502c5
Machiste Quintana <machiste.quintana@yale.edu>
2015-01-30 09:25:19 -0500

    fix select2 display [skip ci]

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -83,6 +83,21 @@ SimpleForm.setup do |config|
     end
   end
 
+  config.wrappers :horizontal_select2, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
+    b.use :html5
+    b.use :placeholder
+    b.optional :maxlength
+    b.optional :readonly
+    b.use :label, class: 'col-sm-2 control-label'
+
+    b.wrapper tag: 'div', class: 'col-sm-8' do |ba|
+      ba.use :input
+      ba.use :error, wrap_with: { tag: 'span', class: 'help-block' }
+      ba.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
+    end
+  end
+
+
   config.wrappers :horizontal_boolean, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
     b.use :html5
     b.optional :readonly
