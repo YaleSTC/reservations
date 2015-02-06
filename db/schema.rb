ae2cbe937fc2de8106ffbd3626107d899862d516
Sydney Young <sydney.young@yale.edu>
2015-02-05 19:25:55 -0500

    add upcoming_checkout_email_active to AppConfig

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141219172429) do
+ActiveRecord::Schema.define(version: 20150206002111) do
 
   create_table "announcements", force: true do |t|
     t.text     "message"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20141219172429) do
     t.boolean  "enable_new_users",                                   default: true
     t.integer  "res_exp_time"
     t.boolean  "enable_guests",                                      default: true
+    t.boolean  "upcoming_checkout_email_active"
   end
 
   create_table "blackouts", force: true do |t|
