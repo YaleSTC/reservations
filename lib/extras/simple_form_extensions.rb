c444f825329593d02b783e2db0ff5358caa1142a
Machiste Quintana <mnquintana@users.noreply.github.com>
2015-02-15 20:50:50 -0500

    Fix form button offset once and for all

--- lib/extras/simple_form_extensions.rb
+++ lib/extras/simple_form_extensions.rb
@@ -1,31 +1,15 @@
 module ButtonComponents
   def submit_button(*args, &block)
     options = args.extract_options!
-    # This was breaking the contact form - better to leave commented for now
-    # if object.new_record?
-    #   loading = I18n.t('simple_form.creating')
-    # else
-    #   loading = I18n.t('simple_form.updating')
-    # end
-    # options[:"data-loading-text"] =
-    # [loading, options[:"data-loading-text"]].compact
     options[:class] = ['btn-primary', options[:class]].compact
     args << options
     # rubocop:disable AssignmentInCondition
     if cancel = options.delete(:cancel)
-      template.content_tag :div, class: 'form-group' do
-        template.content_tag :div, class: 'col-sm-offset-2 col-sm-10' do
-          submit(*args, &block) + ' ' + template.link_to(
-          template.button_tag(I18n.t('simple_form.buttons.cancel'),
-                              type: 'button', class: 'btn btn-default'), cancel)
-        end
-      end
+      submit(*args, &block) + ' ' + template.link_to(
+      template.button_tag(I18n.t('simple_form.buttons.cancel'),
+                          type: 'button', class: 'btn btn-default'), cancel)
     else
-      template.content_tag :div, class: 'form-group' do
-        template.content_tag :div, class: 'col-sm-offset-2 col-sm-10' do
-          submit(*args, &block)
-        end
-      end
+      submit(*args, &block)
     end
     # rubocop:enable AssignmentInCondition
   end
