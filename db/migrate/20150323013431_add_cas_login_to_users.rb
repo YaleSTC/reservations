f97e9f9d12a44e3c9a9774d660d2a6c94c859f60
Oren Kanner <okanner@gmail.com>
2015-10-17 23:42:27 -0400

    Actually fix authentication initializer
    Resolves #1324 (and #1282 for real)
    - ensures that authentication initializer actually runs
    - ensures that username is copied to cas_login during migration if
      necessary

--- db/migrate/20150323013431_add_cas_login_to_users.rb
+++ db/migrate/20150323013431_add_cas_login_to_users.rb
@@ -1,5 +1,11 @@
 class AddCasLoginToUsers < ActiveRecord::Migration
   def change
     add_column :users, :cas_login, :string
+
+    # copy username to cas_login if you use CAS already
+    if ENV['CAS_AUTH']
+      ActiveRecord::Base.connection
+        .execute('update users set cas_login=username')
+    end
   end
 end
