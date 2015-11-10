5b9143020721a4b984609a5b0c3da1ebfe77c532
Oren Kanner <okanner@gmail.com>
2015-11-09 21:14:07 -0500

    Move Heroku deployment gems into separate heroku group
    Resolves #1345
    - fix issue with missing logs
    - add extra Gemfile group / env variable to app.json
    - update Bundler call in application.rb

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,7 @@ require 'rails/all'
 
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env)
+Bundler.require(*Rails.groups(:default, Rails.env))
 
 module Reservations
   class Application < Rails::Application
