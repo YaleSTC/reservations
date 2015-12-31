21de963a1159ebc897ac7f89a396afbccab3590e
Oren Kanner <okanner@gmail.com>
2015-12-31 00:10:09 -0500

    Add affiliation handling to LDAP lookup
    Resolves #1375
    - accept new env variable with comma-separated parameters, and return
      concatenated results

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -168,7 +168,7 @@ $(document).ready(function() {
   }
 
   $('#modal').click(function() {
-    $('#userModal div.modal-body').load(new_user, {possible_netid: $('#fake_reserver_id').val() }); // new_user defined in variables.js.erb
+    $('#userModal div.modal-body').load(new_user, {possible_login: $('#fake_reserver_id').val() }); // new_user defined in variables.js.erb
   });
 
   load_datepicker();
