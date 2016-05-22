88b035a2e0f5e15e16a4de8ca80c90eec44993eb
Oren Kanner <okanner@gmail.com>
2016-05-22 11:01:37 -0400

    Add Rails Admin auditing (WIP)

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -12,7 +12,7 @@ RailsAdmin.config do |config|
 
   ## == PaperTrail ==
   # PaperTrail >= 3.0.0
-  # config.audit_with :paper_trail, 'User', 'PaperTrail::Version'
+  config.audit_with :paper_trail, 'User', 'PaperTrail::Version'
 
   ### More at https://github.com/sferik/rails_admin/wiki/Base-configuration
 
