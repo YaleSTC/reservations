aa930bf1b37c301c52fbe52766bd28f8f15060d4
Sydney Young <sydney.young@yale.edu>
2016-06-02 11:05:48 -0400

    Fix intermittent asset failure in specs
    Resolves #1528
      - Mock relative root in application helper specs

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -11,7 +11,8 @@ describe ApplicationHelper, type: :helper do
     context 'when passed a valid object' do
       it 'returns the url if no relative root is set' do
         good_obj = double(url: '/url')
-        Rails.application.config.action_controller.relative_url_root = nil
+        allow(Rails.application.config.action_controller).to \
+          receive(:relative_url_root).and_return(nil)
 
         expect(paperclip_full_url(good_obj)).to eq(good_obj.url)
       end
@@ -19,7 +20,8 @@ describe ApplicationHelper, type: :helper do
       it 'returns the url prepended by the relative root if set' do
         good_obj = double(url: '/url')
         rel_root = 'test'
-        Rails.application.config.action_controller.relative_url_root = rel_root
+        allow(Rails.application.config.action_controller).to \
+          receive(:relative_url_root).and_return(rel_root)
 
         expect(paperclip_full_url(good_obj)).to \
           eq("#{rel_root}#{good_obj.url}")
