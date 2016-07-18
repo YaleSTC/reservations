92c0040ce538725164009f986a11dac5aa8ab202
Sydney Young <sydney.young@yale.edu>
2016-07-18 19:09:39 -0400

    Refactor EquipmentItem Model spec
    Resolves #1610

--- spec/factories/equipment_items.rb
+++ spec/factories/equipment_items.rb
@@ -11,7 +11,7 @@ FactoryGirl.define do
     deactivation_reason nil
     notes ''
 
-    factory :deactivated do
+    factory :deactivated_item do
       deactivation_reason 'Because I can'
       deleted_at '2013-01-01 00:00:00'
     end
