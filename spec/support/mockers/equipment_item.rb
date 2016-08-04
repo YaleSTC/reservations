e23bf233e65ef1990163512cf8fe5bea3cd1d9b3
Sydney Young <sydney.young@yale.edu>
2016-08-04 08:55:55 -0400

    Refactor Requirements Controller Spec
    Resolves #1594

---
+++ spec/support/mockers/equipment_item.rb
@@ -0,0 +1,20 @@
+require Rails.root.join('spec/support/mockers/mocker.rb')
+require Rails.root.join('spec/support/mockers/equipment_model.rb')
+
+class EquipmentItemMock < Mocker
+  def self.klass
+    EquipmentItem
+  end
+
+  def self.klass_name
+    'EquipmentItem'
+  end
+
+  private
+
+  def with_model(model: nil)
+    model ||= EquipmentModelMock.new
+    child_of_has_many(mocked_parent: model, parent_sym: :equipment_model,
+                      child_sym: :equipment_items)
+  end
+end
---
+++ spec/support/mockers/requirement.rb
@@ -0,0 +1,11 @@
+require Rails.root.join('spec/support/mockers/mocker.rb')
+
+class RequirementMock < Mocker
+  def self.klass
+    Requirement
+  end
+
+  def self.klass_name
+    'Requirement'
+  end
+end
