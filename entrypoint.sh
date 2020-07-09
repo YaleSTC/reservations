ee0a6bae5b3fcb36f1b4d51bc5180473dc161704
Neil A Dokurno <neil.dokurno@yale.edu>
2020-07-09 10:21:43 -0400

    Added docker-compose dev setup

---
+++ entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+
+# Remove a potentially pre-existing server.pid for Rails.
+rm -f /app/tmp/pids/server.pid
+
+# Then exec the container's main process (what's set as CMD in the Dockerfile).
+exec "$@"
